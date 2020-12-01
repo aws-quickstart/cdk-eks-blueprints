cca8d908307ac5d33b3fc16b386706522e17a5f4
Mikhail Shapirov <shapirov@amazon.com>
2020-12-01 18:57:39 -0500

    externalized a few configuration options

--- README.md
+++ README.md
@@ -21,5 +21,12 @@ In addition to the regular [environment bootstrapping](https://docs.aws.amazon.c
 
 
 # cdk-eks-blueprint
+Support context variables (specify in cdk.json, cdk.context.json or pass with -c command line option):
+
+- instanceType: (defaulted to "t3.medium") Type of instance for the EKS cluster, must be a valid instance type like t3.medium
+- vpc: Specifies whether to use an existing VPC (if specified) or create a new one if not specified.
+- minSize: Min cluster size, must be positive integer greater than 0 (default 1).
+- maxSize: Max cluster size, must be greater than minSize.
+- vpcSubnets ="List of VPC subnets for cluster provisioning (unsupported yet)
 
 
--- lib/eksBlueprintStack.ts
+++ lib/eksBlueprintStack.ts
@@ -7,42 +7,65 @@ import { StackProps } from '@aws-cdk/core';
 export class CdkEksBlueprintStack extends cdk.Stack {
 
     cluster: eks.Cluster;
-    vpc: ec2.Vpc;
+    vpc: ec2.IVpc;
     nodeGroup: eks.Nodegroup;
 
-    constructor(scope: cdk.Construct, id: string, addOns?: Array<ClusterAddOn>, teams? : Array<TeamSetup>, props?: StackProps ) {
+
+    constructor(scope: cdk.Construct, id: string, addOns?: Array<ClusterAddOn>, teams?: Array<TeamSetup>, props?: StackProps) {
         super(scope, id, props);
-        
+        /*
+         * Supported parameters
+        */
+        const instanceType = this.node.tryGetContext("instanceType") ?? 't3.medium';
+        const vpcId = this.node.tryGetContext("vpc");
+        const minClusterSize = this.node.tryGetContext("minSize") ?? 1;
+        const maxClusterSize = this.node.tryGetContext("maxSize") ?? 3;
+        const vpcSubnets = this.node.tryGetContext("vpcSubnets");
+
+
         // It will automatically divide the provided VPC CIDR range, and create public and private subnets per Availability Zone.
         // Network routing for the public subnets will be configured to allow outbound access directly via an Internet Gateway.
         // Network routing for the private subnets will be configured to allow outbound access via a set of resilient NAT Gateways (one per AZ).
-        this.vpc = new ec2.Vpc(this, id + "-vpc");
+        if(vpcId != null) {
+            this.vpc = ec2.Vpc.fromLookup(this, id + "-vpc", { vpcId: vpcId });
+        }
+
+        if(this.vpc == null) {
+            this.vpc = new ec2.Vpc(this, id + "-vpc");
+        }
 
         this.cluster = new eks.Cluster(this, id, {
             vpc: this.vpc,
             clusterName: id,
             outputClusterName: true,
             defaultCapacity: 0, // we want to manage capacity ourselves
-            version: eks.KubernetesVersion.V1_17,
+            version: eks.KubernetesVersion.V1_18,
+            vpcSubnets: undefined
         });
 
 
         this.nodeGroup = this.cluster.addNodegroupCapacity(id + "-ng", {
-            instanceType: new ec2.InstanceType("t3.medium"),
-            minSize: 1,
-            maxSize: 4
+            instanceType: new ec2.InstanceType(instanceType),
+            minSize: minClusterSize,
+            maxSize: maxClusterSize
         });
 
-        if(addOns) {
-            for(let addOn of addOns) { // must iterate in the strict order
+        if (addOns) {
+            for (let addOn of addOns) { // must iterate in the strict order
                 addOn.deploy(this);
             }
         }
 
-        if(teams) {
+        if (teams) {
             teams.forEach(team => team.setup(this));
         }
     }
+
+    defineParameters() {
+
+
+    }
+
 }
 
 export interface ClusterAddOn {
