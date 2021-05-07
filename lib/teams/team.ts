9c12eaeef96b9544fb8eb1f7cdd1800aa00a4a7c
shapirov103 <shapirov@amazon.com>
2021-05-07 00:21:15 -0400

    incremental change for auth added master roles and users as well as team role and users

--- lib/teams/team.ts
+++ lib/teams/team.ts
@@ -1,4 +1,8 @@
+import { UntrustedCodeBoundaryPolicy } from "@aws-cdk/aws-codebuild";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
 import { Role, User } from "@aws-cdk/aws-iam";
+import { AnyPrincipal, ArnPrincipal, CompositePrincipal } from "@aws-cdk/aws-iam/lib/principals";
+import { unwatchFile } from "fs";
 import { ClusterInfo, TeamSetup } from "../eksBlueprintStack";
 
 export class TeamProps {
@@ -21,12 +25,43 @@ export class Team implements TeamSetup {
     }
 
     setup(clusterInfo: ClusterInfo): void {
+        const props = this.teamProps;
+        const awsAuth = clusterInfo.cluster.awsAuth;
+        const admins = this.teamProps.adminUser ? [this.teamProps.adminUser] : [];
+        const adminRole = this.getOrCreateRole(clusterInfo, admins, props.adminUserRole);
 
-        if(this.teamProps.adminUserRole)
-            clusterInfo.cluster.awsAuth.addMastersRole(this.teamProps.adminUserRole);
+        if(adminRole) {
+            awsAuth.addMastersRole(adminRole);
+        }
+        
+        const users = this.teamProps.users ?? [];
+        const teamRole = this.getOrCreateRole(clusterInfo, users, props.userRole);
 
+        if(teamRole) {
+            awsAuth.addRoleMapping(teamRole, {groups: [props.name + "-group"], username: props.name});
+        }
     }
 
+    private getOrCreateRole(clusterInfo: ClusterInfo,  users: Array<User>, role? : Role) : Role | undefined {   
+        if(users?.length == 0) {
+            return role;
+        }
+        
+        let principals = users?.map(item => new ArnPrincipal(item.userArn));
 
+        if(role) {
+            role.assumeRolePolicy?.addStatements(
+                new PolicyStatement({
+                    principals: principals
+                })
+            );
+        }
+        else {
+            role = new Role(clusterInfo.cluster, clusterInfo.cluster.clusterName + "Admin", {
+                assumedBy: new CompositePrincipal(...principals)
+            });
+        }
 
+        return role;
+    }
 }
