2375061cdffbc9fa1dfda66498f5a61928b7c1b7
shapirov103 <shapirov@amazon.com>
2021-05-10 00:17:01 -0400

    split regular vs admin team and added namespace and quota handling

--- lib/teams/team.ts
+++ lib/teams/team.ts
@@ -1,17 +1,48 @@
-import { UntrustedCodeBoundaryPolicy } from "@aws-cdk/aws-codebuild";
 import { PolicyStatement } from "@aws-cdk/aws-iam";
 import * as iam from '@aws-cdk/aws-iam';
 import { ClusterInfo, TeamSetup } from "../stacks/eks-blueprint-stack";
 import { CfnOutput } from "@aws-cdk/core";
 
+/**
+ * Team properties.
+ */
 export class TeamProps {
 
+    /**
+     * Required unique name for organization.
+     * May map to an OU name. 
+     */
     readonly name: string;
 
-    readonly adminUser?: iam.User;
-    readonly adminUserRole?: iam.Role;
+    /**
+     * Defaults to team name prefixed by "team-"
+     */
+    readonly namespace?: string;
 
+    /**
+     *  Annotations such as necessary for GitOps engine. 
+     */
+    readonly namespaceAnnotations?= { "argocd.argoproj.io/sync-wave": "-1" };
+
+    /**
+     * Optional, but highly recommended setting to ensure predictable demands.
+     */
+    readonly namespaceHardLimits? = {
+        'requests.cpu': '10', // TODO verify sane defaults
+        'requests.memory': '10Gi',
+        'limits.cpu': '20',
+        'limits.memory': '20Gi'
+    }
+
+    /**
+     *  Team members who need to get access to the cluster
+     */
     readonly users?: Array<iam.User>;
+
+    /**
+     * Options existing role that should be used for cluster access. 
+     * If userRole and users are not provided, then no IAM setup is performed. 
+     */
     readonly userRole?: iam.Role;
 }
 
@@ -25,29 +56,37 @@ export class Team implements TeamSetup {
 
     setup(clusterInfo: ClusterInfo): void {
         this.defaultSetupAccess(clusterInfo);
+        this.setupNamespace(clusterInfo);
     }
 
     defaultSetupAccess(clusterInfo: ClusterInfo) {
         const props = this.teamProps;
         const awsAuth = clusterInfo.cluster.awsAuth;
-        const admins = this.teamProps.adminUser ? [this.teamProps.adminUser] : [];
-        const adminRole = this.getOrCreateRole(clusterInfo, admins, props.adminUserRole);
-
-        new CfnOutput(clusterInfo.cluster.stack, props.name + ' team admin ', { value: adminRole.roleArn })
-
-        if (adminRole) {
-            awsAuth.addMastersRole(adminRole);
-        }
 
         const users = this.teamProps.users ?? [];
         const teamRole = this.getOrCreateRole(clusterInfo, users, props.userRole);
 
         if (teamRole) {
             awsAuth.addRoleMapping(teamRole, { groups: [props.name + "-group"], username: props.name });
+            new CfnOutput(clusterInfo.cluster.stack, props.name + ' team role ', { value: teamRole ? teamRole.roleArn : "none" })
         }
     }
 
-    private getOrCreateRole(clusterInfo: ClusterInfo, users: Array<User>, role?: Role): Role | undefined {
+    defaultSetupAdminAccess(clusterInfo: ClusterInfo) {
+        const props = this.teamProps;
+        const awsAuth = clusterInfo.cluster.awsAuth;
+        const admins = this.teamProps.users ?? [];
+        const adminRole = this.getOrCreateRole(clusterInfo, admins, props.userRole);
+
+        new CfnOutput(clusterInfo.cluster.stack, props.name + ' team admin ', { value: adminRole ? adminRole.roleArn : "none" })
+
+        if (adminRole) {
+            awsAuth.addMastersRole(adminRole);
+        }
+    }
+
+
+    getOrCreateRole(clusterInfo: ClusterInfo, users: Array<iam.User>, role?: iam.Role): iam.Role | undefined {
         if (users?.length == 0) {
             return role;
         }
@@ -66,7 +105,7 @@ export class Team implements TeamSetup {
                 assumedBy: new iam.CompositePrincipal(...principals)
             });
             role.addToPolicy(new iam.PolicyStatement({
-                effect: iam.Effect.,
+                effect: iam.Effect.ALLOW,
                 resources: [clusterInfo.cluster.clusterArn],
                 actions: [
                     "eks:DescribeNodegroup",
@@ -78,10 +117,53 @@ export class Team implements TeamSetup {
                     "eks:ListUpdates",
                     "eks:ListFargateProfiles"
                 ]
-              })
+            })
             );
-    }
+        }
 
         return role;
     }
+
+    setupNamespace(clusterInfo: ClusterInfo) {
+        const props = this.teamProps;
+        const namespaceName = props.namespace ?? "team-" + props.name;
+        const namespace = clusterInfo.cluster.addManifest(props.name, {
+            apiVersion: 'v1',
+            kind: 'Namespace',
+            metadata: {
+                name: namespaceName,
+                annotations: props.namespaceAnnotations
+            }
+        });
+
+        if(props.namespaceHardLimits) {
+            this.setupNamespacePolicies(clusterInfo, namespaceName);
+        }
+    }
+
+    setupNamespacePolicies(clusterInfo: ClusterInfo, namespaceName: string) {
+        const quotaName = this.teamProps.name + "-quota";
+        clusterInfo.cluster.addManifest(quotaName, {
+            apiVersion: 'v1',
+            kind: 'ResourceQuota',
+            metadata: { 
+                name: quotaName,
+                namespace: namespaceName
+            },
+            spec: {
+                hard: this.teamProps.namespaceHardLimits
+            }
+        });
+    }
+}
+
+/**
+ * Platform team will setup all team members as admin access to the cluster by adding them to the master group.
+ */
+export class PlatformTeam extends Team {
+
+    setup(clusterInfo: ClusterInfo): void {
+        this.defaultSetupAdminAccess(clusterInfo);
+    }
+
 }
