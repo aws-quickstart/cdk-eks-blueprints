d3fbe8a4b4bc900f478338020d1589be8568aa03
Mikhail Shapirov <shapirov@amazon.com>
2021-05-25 17:03:20 -0400

    initial defaults and context variables

--- lib/cluster-providers/ec2-cluster-provider.ts
+++ lib/cluster-providers/ec2-cluster-provider.ts
@@ -1,8 +1,11 @@
 import { Construct } from "@aws-cdk/core";
-import { InstanceType, IVpc, SubnetSelection } from "@aws-cdk/aws-ec2";
+import { InstanceType, InstanceClass,  InstanceSize, IVpc, SubnetSelection } from "@aws-cdk/aws-ec2";
 import { Cluster, CommonClusterOptions, KubernetesVersion, NodegroupAmiType } from "@aws-cdk/aws-eks";
-
 import { ClusterInfo, ClusterProvider } from "../stacks/eks-blueprint-stack";
+
+const DEFAULT_INSTANCE_TYPE = InstanceType.of(InstanceClass.M5, InstanceSize.LARGE);
+const DEFAULT_NG_MINSIZE = 1;
+const DEFAULT_NG_MAXSIZE = 3;
 
 export interface EC2ProviderClusterProps extends CommonClusterOptions {
     instanceType?: InstanceType; // m5.large
@@ -16,32 +19,44 @@ export interface EC2ProviderClusterProps extends CommonClusterOptions {
     vpcSubnets?: SubnetSelection[];
 }
 
+function  defaultValue(construct: Construct, key: string, defaultValue : any) {
+    return construct.node.tryGetContext(key) ?? defaultValue;
+}
+
+/**
+ * Base implementation of an EC2 cluster provider with managed node group.
+ */
 export class EC2ClusterProvider implements ClusterProvider {
 
-    readonly providerOptions: EC2ProviderClusterProps;
+    readonly options: EC2ProviderClusterProps;
+
 
     constructor(options?: EC2ProviderClusterProps) {
-        this.providerOptions = options ?? { version: KubernetesVersion.V1_19 };
+        this.options = options ?? { version: KubernetesVersion.V1_19 };
     }
 
     createCluster(scope: Construct, vpc: IVpc, version: KubernetesVersion): ClusterInfo {
 
         const id = scope.node.id;
 
+        const instanceType = this.options.instanceType ?? defaultValue(scope, "eks.default.instance-type", DEFAULT_INSTANCE_TYPE);
+        const minSize  = this.options.minSize ?? defaultValue(scope, "eks.default.min-size", DEFAULT_NG_MINSIZE);
+        const maxSize = this.options.maxSize ?? defaultValue(scope, "eks.default.max-size", DEFAULT_NG_MAXSIZE);
+        
         const cluster = new Cluster(scope, id, {
             vpc: vpc,
             clusterName: id,
             outputClusterName: true,
             defaultCapacity: 0, // we want to manage capacity ourselves
-            version: this.providerOptions.version,
-            vpcSubnets: this.providerOptions.vpcSubnets,
-        });
+            version: this.options.version,
+            vpcSubnets: this.options.vpcSubnets,
+        });   
 
         const nodeGroup = cluster.addNodegroupCapacity(id + "-ng", {
-            instanceType: this.providerOptions.instanceType,
-            amiType: this.providerOptions.amiType,
-            minSize: this.providerOptions.minSize,
-            maxSize: this.providerOptions.maxSize
+            instanceType: instanceType,
+            amiType: this.options.amiType,
+            minSize: minSize,
+            maxSize: maxSize
         });
 
         return { cluster: cluster, nodeGroup: nodeGroup, version: version };
