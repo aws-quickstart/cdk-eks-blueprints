df3e9f4732ab7b4c30a85e0bd3a86fb2a823933b
Andrew Park <andrewpark93@hotmail.com>
2021-07-06 23:10:10 -0700

    pushing updates

---
+++ argocd-boostrap/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo "checking ArgoCD version on the server"
+argocd version --short --client  
+
+echo "change argocd-server service type to load balancer"
+export ARGOCD_SERVER=$(kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}')
+
+echo "set argocd password as env variable" 
+export ARGO_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
+
+
+echo "login to the server and change password"
+argocd login $ARGOCD_SERVER --username admin --password $ARGO_PWD --insecure
+
+
+echo "mount secret obtained in step above and mount it as a secret to this job"
+echo "updated argocd password"
+argocd account update-password
+echo "create EKS cluster configuration"
+CONTEXT_NAME=`kubectl config view -o jsonpath='{.contexts[].name}'`
+argocd cluster add $CONTEXT_NAME
+echo "bootstrap app of apps"
+argocd app create apps --dest-namespace argocd  --dest-server https://kubernetes.default.svc  --repo git@github.com:shapirov103/argo-apps.git --path "."
+argocd app sync apps     
