c44ec9ce84fd8924d2710522080b408355c431d1
Mikhail Shapirov <shapirov@amazon.com>
2021-07-08 19:11:20 -0400

    better support for route53 for external DNS and NGINX

--- lib/addons/external-dns/index.ts
+++ lib/addons/external-dns/index.ts
@@ -1,7 +1,8 @@
 import { KubernetesManifest } from '@aws-cdk/aws-eks';
-import { Effect, PolicyStatement } from '@aws-cdk/aws-iam';
-import { IHostedZone } from '@aws-cdk/aws-route53';
+import { Effect, PolicyStatement, Role } from '@aws-cdk/aws-iam';
+import { IHostedZone, HostedZone, PublicHostedZone, CrossAccountZoneDelegationRecord, CnameRecord} from '@aws-cdk/aws-route53';
 import { ClusterAddOn, ClusterInfo } from '../../stacks/cluster-types';
+
 
 /**
  * Configuration options for the external DNS add-on.
@@ -22,7 +23,72 @@ export interface ExternalDnsProps {
 }
 
 
-export type HostedZoneProvider = (clusterInfo: ClusterInfo) => IHostedZone[];
+export interface HostedZoneProvider {
+    provide(clusterInfo: ClusterInfo): IHostedZone[];
+}
+
+/**
+ * Simple lookup host zone provider
+ */
+export class LookupHostedZoneProvider implements HostedZoneProvider {
+
+    /**
+     * @param hostedZoneName name of the host zone to lookup
+     * @param id  optional id for the structure (for tracking). set to hostzonename by default
+     */
+    constructor(private hostedZoneName: string, private id?: string) { }
+
+    provide(clusterInfo: ClusterInfo): IHostedZone[] {
+        return [HostedZone.fromLookup(clusterInfo.cluster.stack, this.id ?? `${this.hostedZoneName}-Lookup`, { domainName: this.hostedZoneName })];
+    }
+}
+
+export class DelegatingHostedZoneProvider implements HostedZoneProvider {
+    constructor(private parentDomain: string,
+        private subdomain: string,
+        private parentDnsAccountId: string,
+        private delegatingRoleName: string,
+        private wildcardSubdomain? : boolean) { }
+
+    provide(clusterInfo: ClusterInfo): IHostedZone[] {
+        const stack = clusterInfo.cluster.stack;
+
+        const subZone = new PublicHostedZone(stack, `${this.subdomain}-SubZone`, {
+            zoneName: this.subdomain
+        });
+        
+        if (this.wildcardSubdomain) {
+            new CnameRecord(stack, `${this.subdomain}-cname`, {
+                zone: subZone,
+                domainName: `*.{subdomain}`
+            });
+        }
+
+        // 
+        // import the delegation role by constructing the roleArn.
+        // Assuming the parent account has the delegating role with 
+        // trust relationship setup to the child account.
+        //
+        const delegationRoleArn = stack.formatArn({
+            region: '', // IAM is global in each partition
+            service: 'iam',
+            account: this.parentDnsAccountId,
+            resource: 'role',
+            resourceName: this.delegatingRoleName
+        });
+
+        const delegationRole = Role.fromRoleArn(stack, 'DelegationRole', delegationRoleArn);
+
+        // create the record
+        new CrossAccountZoneDelegationRecord(stack, `${this.subdomain}-delegate`, {
+            delegatedZone: subZone,
+            parentHostedZoneName: this.parentDomain, // or you can use parentHostedZoneId
+            delegationRole,
+        });
+
+        return [subZone];
+    }
+}
 
 /**
  * Implementation of the External DNS service: https://github.com/kubernetes-sigs/external-dns/.
@@ -42,17 +108,18 @@ export class ExternalDnsAddon implements ClusterAddOn {
 
         const namespaceManifest = new KubernetesManifest(cluster.stack, 'external-dns-ns', {
             cluster,
-            manifest: [{ apiVersion: 'v1',
+            manifest: [{
+                apiVersion: 'v1',
                 kind: 'Namespace',
                 metadata: { name: namespace },
             }],
             overwrite: true
         });
-        
+
         const sa = cluster.addServiceAccount(this.name, { name: 'external-dns-sa', namespace });
 
-        const hostedZones = this.options.hostedZone(clusterInfo);
-  
+        const hostedZones = this.options.hostedZone.provide(clusterInfo);
+
         sa.addToPrincipalPolicy(
             new PolicyStatement({
                 effect: Effect.ALLOW,
