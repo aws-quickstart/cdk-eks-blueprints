25ddf30473a68596637b4907972f3e1668f7fc8e
Mikhail Shapirov <shapirov@amazon.com>
2021-07-19 19:45:39 -0400

    argo bootstrap, wip

--- lib/addons/argocd/index.ts
+++ lib/addons/argocd/index.ts
@@ -1,26 +1,139 @@
+import { HelmChart, KubernetesManifest } from "@aws-cdk/aws-eks";
 import { ClusterAddOn, ClusterInfo, ClusterPostDeploy } from "../../stacks/cluster-types";
 import { Team } from "../../teams";
 
+export interface ArgoApplicationRepository {
+    /**
+     * Expected to support helm style repo at the moment
+     */
+    repoUrl: string,
+
+    /** 
+     * Path within the repository 
+     */
+    path?: string,
+
+    /**
+     * Optional name for the bootstrap application
+     */
+    name?: string,
+
+    /**
+     * Secret reference (partial arn) to pull the secret.
+     * 
+     */
+    credentialsArn: string,
+
+    /**
+     * Depending on credentials type the arn should either point to an SSH key
+     * or a json 
+     */
+    credentialsType: "USERNAME" | "TOKEN" | "SSH"
+
+}
+
+/**
+ * Configuration options for ArgoCD add-on.
+ */
+export interface ArgoCDAddOnProps {
+    namespace?: string,
+    /**
+     * If provided, the addon will bootstrap the app or apps in the provided repository.
+     * In general, the repo is expected to have the app of apps, which can enable to bootstrap all workloads,
+     * after the infrastructure and team provisioning is complete. 
+     */
+    bootstrapRepo?: ArgoApplicationRepository
+}
+
+const argoDefaults: ArgoCDAddOnProps = {
+    namespace: "arogcd"
+}
 export class ArgoCDAddOn implements ClusterAddOn, ClusterPostDeploy {
 
-    readonly namespace: string;
+    readonly options: ArgoCDAddOnProps;
+    private chartNode: HelmChart;
 
-    constructor(namespace?: string) {
-        this.namespace = namespace ?? "argocd";
+    constructor(props: ArgoCDAddOnProps) {
+        this.options = { ...argoDefaults, ...props };
     }
 
     deploy(clusterInfo: ClusterInfo): void {
-        clusterInfo.cluster.addHelmChart("argocd-addon", {
+        this.chartNode = clusterInfo.cluster.addHelmChart("argocd-addon", {
             chart: "argo-cd",
-            
+            release: "ssp-addon",
             repository: "https://argoproj.github.io/argo-helm",
             version: '3.2.3',
-            namespace: this.namespace,
+            namespace: this.options.namespace
         });
     }
 
     postDeploy(clusterInfo: ClusterInfo, teams: Team[]): void {
-        //TODO: implement this method
-        console.assert(clusterInfo !== null && teams !== null);
+        const appRepo = this.options.bootstrapRepo;
+        if(!appRepo) {
+            return;
+        }
+        if(appRepo.sshKeyArn) {
+            this.createSecretKey(appRepo, clusterInfo);
+        }
+
+        const manifest = new KubernetesManifest(clusterInfo.cluster.stack, "bootstrap-app", {
+            cluster: clusterInfo.cluster,
+            manifest : [{
+                apiVersion: "argoproj.io/v1alpha1",
+                kind: "Application",
+                metadata: {
+                    name: appRepo.name ?? "bootstrap-apps",
+                    namespace: this.options.namespace
+                },
+                spec: {
+                    destination:{
+                        namespace: "default", 
+                        server: "https://kubernetes.default.svc"
+                    },
+                    project: "default",
+                    source: {
+                        helm: {
+                            valueFiles: ["values.yaml"]
+                        },
+                        path: appRepo.path,
+                        repoURL: appRepo.repoUrl,
+                        targetRevision: "HEAD"
+                    },
+                    syncPolicy: {
+                        automated: {}
+                    }
+                }
+            }],
+            overwrite: true,
+            prune: true  
+        });
+        //
+        // Make sure the bootstrap is only applied after successful ArgoCD installation.
+        //
+        manifest.node.addDependency(this.chartNode);
+    }
+
+    createSecretKey(clusterInfo : ClusterInfo) {
+        const appRepo = this.options.bootstrapRepo;
+
+        new KubernetesManifest(clusterInfo.cluster.stack, "argo-bootstrap-secret", {
+            cluster: clusterInfo.cluster,
+            manifest: [{
+                apiVersion: "v1",
+                kind: "Secret", 
+                metadata: {
+                  name: appRepo?.name?? "bootstrap-repo-secret",
+                  namespace: this.options.namespace,
+                  labels: {
+                    "argocd.argoproj.io/secret-type": "repository"
+                  }
+                },
+                stringData: {
+                  url: appRepo.repoUrl
+                }
+            }],
+            overwrite: true,
+            prune: true
+        })
     }
 }
