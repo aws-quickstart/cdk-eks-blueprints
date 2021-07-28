a36f79d0c94f2be8eb46c7f855ddae934c9847e8
Andrew Park <andrewpark93@hotmail.com>
2021-07-28 13:00:44 -0700

    revised version and namespace for add on

--- lib/addons/OPA/index.ts
+++ lib/addons/OPA/index.ts
@@ -4,9 +4,9 @@ export class OpaAddOn implements OpaAddOn {
 
     deploy(clusterInfo: ClusterInfo): void {
         clusterInfo.cluster.addHelmChart("OPA-addon", {
-            chart: "aws-calico",
-            release: "aws-calico",
-            repository: "https://aws.github.io/eks-charts",
+            chart: "gatekeeper",
+            release: "gatekeeper",
+            repository: "https://open-policy-agent.github.io/gatekeeper/charts",
             version: "3.6.0-beta.2",
             namespace: "kube-system"
         });
