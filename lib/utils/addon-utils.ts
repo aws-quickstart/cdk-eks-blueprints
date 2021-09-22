d741c8ed5a888726b6f9d1f60920ff6ddd4bb7b0
Apoorva Kulkarni <kuapoorv@amazon.com>
2021-09-21 20:23:30 -0700

    update assert message

--- lib/utils/addon-utils.ts
+++ lib/utils/addon-utils.ts
@@ -11,22 +11,30 @@ export function getAddOnNameOrId(addOn: ClusterAddOn): string {
   return addOn.id ?? addOn.constructor.name;
 }
 
+/**
+ * Decorator function that accepts a list of AddOns and
+ * ensures addons are scheduled to be added as well as
+ * add them as dependencies
+ * @param addOns 
+ * @returns 
+ */
 export function dependable(addOns: Array<string>) {
   // eslint-disable-next-line @typescript-eslint/ban-types
   return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor) {
-    const original = descriptor.value;
+    const originalMethod = descriptor.value;
 
     descriptor.value = function( ...args: any[]) {
       const dependencies = Array<Promise<Construct>>();
       const clusterInfo: ClusterInfo = args[0];
+      const stack = clusterInfo.cluster.stack.stackName;
 
       addOns.forEach( (addOn) => {
         const dep = clusterInfo.getScheduledAddOn(addOn);
-        console.assert(dep, `Missing dependency for ${addOn}`);
+        console.assert(dep, `Missing a dependency for ${addOn} for ${stack}`);
         dependencies.push(dep!);
       });
 
-      const result: Promise<Construct> = original.apply(this, args);
+      const result: Promise<Construct> = originalMethod.apply(this, args);
 
       Promise.all(dependencies.values()).then((constructs) => {
         constructs.forEach((construct) => {
