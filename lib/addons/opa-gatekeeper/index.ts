8478e19ac68d88f111babbd944a6ca6cc07e0371
Andrew Park <andrewpark93@hotmail.com>
2021-09-22 11:29:21 -0700

    passing values manually to helm chart based on open issue

--- lib/addons/opa-gatekeeper/index.ts
+++ lib/addons/opa-gatekeeper/index.ts
@@ -1,40 +1,44 @@
 import { ClusterAddOn, ClusterInfo } from "../../spi";
+
 /**
  * Properties available to configure opa gatekeeper
  */
 export interface OpaGatekeeperAddOnProps {
     /**
-     * Namespace where OPA Gatekeeper will be installed
+     * Create namespace for Gatekeeper controller 
      * @default true
      */
     createNamespace?: boolean;
+
     /** Default namespace
      * @default gatekeeper-system
      */
     namespace?: string;
+
     /**
      * Helm chart version to use to install.
      * @default 3.7.0-beta.1
      */
     version?: string;
+
     /**
      * 	Whether or not Helm should wait until all Pods, PVCs, Services, and minimum number of Pods of a Deployment, StatefulSet, or ReplicaSet are in a ready state before marking the release as successful.
      * @default true
      */
     wait?: boolean;
+
     /**
      * Setting validatingWebhookFailurePolicy to True
      * @default true 
      */
     disableValidatingWebhook?: boolean;
-    /**
-     * Remove post install 
-     * 
-     */
+
+
      values?: {
         [key: string]: any;
     };
 }
+
 /**
  * Defaults options for the add-on
  */
@@ -52,18 +56,25 @@ export class OpaGatekeeperAddOn implements ClusterAddOn {
     
     constructor(props?: OpaGatekeeperAddOnProps) {
         this.options = { ...defaultProps, ...props };
+        //this.options = props ?? defaultProps;
     }
 
     deploy(clusterInfo: ClusterInfo): void {
+
         const props = this.options;
-        const values = props.values ?? {}
+       // const values = props.values ?? {}
+
         clusterInfo.cluster.addHelmChart("opagatekeeper-addon", {
             chart: "gatekeeper",
             release: "gatekeeper",
             repository: "https://open-policy-agent.github.io/gatekeeper/charts",
-            version: this.options.version,
-            namespace: this.options.namespace,
-            values
+            version: props.version,
+            namespace: props.namespace,
+            values: {
+                labelNamespace: 'false',
+                disableValidatingWebhook: 'true',
+                exemptNamespaces: 'kube-system'
+            }
         });
     }
 }
