haofeif/ssp-amazon-eks

added opa gatekeeper to blueprint construct and added defualt configurations

examples/blueprint-construct/index.ts
@@ -50,7 +50,8 @@ export default class BlueprintConstruct extends cdk.Construct {
             new ssp.addons.ContainerInsightsAddOn(),
             new ssp.addons.AwsLoadBalancerControllerAddOn(),
             new ssp.addons.SecretsStoreAddOn(),
-            new ssp.addons.SSMAgentAddOn()
+            new ssp.addons.SSMAgentAddOn(),
+            new ssp.addons.OpaGatekeeperAddOn()
         ];
 
         const blueprintID = `${blueprintProps.id}-dev`

aws-quickstart/cdk-eks-blueprints

examples/blueprint-construct/index.ts
@@ -48,6 +48,7 @@ export default class BlueprintConstruct extends cdk.Construct {
         const addOns: Array<ssp.ClusterAddOn> = [
             new ssp.addons.AppMeshAddOn(),
             prodBootstrapArgo,
+            new ssp.addons.NginxAddOn(),
             new ssp.addons.CalicoAddOn(),
             new ssp.addons.MetricsServerAddOn(),
             new ssp.addons.ClusterAutoScalerAddOn(),
@@ -58,7 +59,8 @@ export default class BlueprintConstruct extends cdk.Construct {
             new ssp.addons.NginxAddOn(),
             new ssp.addons.VpcCniAddOn(),
             new ssp.addons.CoreDnsAddOn(),
-            new ssp.addons.KubeProxyAddOn()
+            new ssp.addons.KubeProxyAddOn(),
+            new ssp.addons.OpaGatekeeperAddOn()
         ];
 
         const blueprintID = `${blueprintProps.id}-dev`;