4efc9bb1366484e7c4552b014603736c79ab7479
Eli <pevetoej@amazon.com>
2022-07-28 15:21:30 -0500

    fixed cdk list issue

--- lib/addons/flagger/index.ts
+++ lib/addons/flagger/index.ts
@@ -1,16 +1,15 @@
-import * as blueprints from '../../';
+import { HelmAddOn, HelmAddOnUserProps, HelmAddOnProps } from "../helm-addon";
+import { ClusterInfo } from "../../spi";
 import { Construct } from 'constructs';
 import { Values } from "../../spi";
 import merge from "ts-deepmerge";
-
 /**
  * User provided options for the FlaggerAddonProps values.
  */
-export interface FlaggerAddOnProps extends blueprints.HelmAddOnUserProps {
+export interface FlaggerAddOnProps extends HelmAddOnUserProps {
   installPrometheus?: boolean;
   meshProvider?: MeshProviderOptions;
 }
-
 /**
  * All the meshProvider values that can be chosen by the user.
  */
@@ -26,11 +25,10 @@ export const enum MeshProviderOptions {
   TRAEFIK = 'traefik',
   OSM = 'osm'
 }
-
 /**
  * defaultProps makes the flagger namespace and chart.
  */
-export const defaultProps: blueprints.HelmAddOnProps & FlaggerAddOnProps = {
+export const defaultProps: HelmAddOnProps & FlaggerAddOnProps = {
   name: "flagger",
   namespace: "flagger",
   chart: "flagger",
@@ -48,24 +46,19 @@ export const defaultProps: blueprints.HelmAddOnProps & FlaggerAddOnProps = {
 /**
  * This creates and deploys a cluster with the FlaggerAddOnProps values for flagger settings with preset values unless the user specifies their own values.
  */
-export class FlaggerAddOn extends blueprints.HelmAddOn {
-
+export class FlaggerAddOn extends HelmAddOn{
   readonly options: FlaggerAddOnProps;
-
   constructor(props?: FlaggerAddOnProps) {
     super({ ...defaultProps, ...props });
     this.options = this.props as FlaggerAddOnProps;
   }
-
-  deploy(clusterInfo: blueprints.ClusterInfo): Promise<Construct> {
-
+  deploy(clusterInfo: ClusterInfo): Promise<Construct> {
     let values: Values = {
       prometheus: {
         install: this.options.installPrometheus ?? defaultProps.installPrometheus
       },
       meshProvider: this.options.meshProvider ?? defaultProps.meshProvider
     };
-
     values = merge(values, this.props.values ?? {});
     const chart = this.addHelmChart(clusterInfo, values);
     return Promise.resolve(chart);
