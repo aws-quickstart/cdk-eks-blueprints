0085d8047c07b15c7024095d924a7db3a83bd7c8
Shardul Vaidya <vshardul@amazon.com>
2022-08-24 02:27:36 -0400

    Started working with setting up the KNative AddOn

---
+++ lib/addons/knative-serving/index.ts
@@ -0,0 +1,67 @@
+import { Construct } from 'constructs';
+import { ClusterAddOn, ClusterInfo, Values } from "../../spi";
+import {KubectlProvider, ManifestConfiguration, ManifestDeployment} from "../helm-addon/kubectl-provider";
+import {dependable, loadExternalYaml, loadYaml, readYamlDocument} from "../../utils";
+import * as cluster from "cluster";
+
+/**
+ * This KNative add-on install the KNative Eventing Core
+ */
+
+/**
+ * Configuration options for add-on
+ */
+export interface KNativeProps extends ManifestConfiguration {
+    /**
+     * Version of KNative Eventing Service that will be used by the Cluster
+     * @default '1.6.0'
+     */
+    version?: string;
+
+    /**
+     * Name of the Repository download endpoint
+     * @default 'knative'
+     */
+    repository?: string;
+
+    /**
+     * Name of the Kubernetes manifest for knative eventing custom resource definitions
+     * @default 'eventing-crds.yaml'
+     */
+    crd_yaml?: string;
+
+    /**
+     * Name of the Kubernetes manifest for knative eventing core
+     * @default 'eventing-core.yaml'
+     */
+    core_yaml?: string;
+}
+
+/**
+ * Defaults options for the add-on
+ */
+const defaultProps = {
+    name: 'knative-event-engine',
+    namespace: 'knative-eventing',
+    repository: 'knative',
+    crd_yaml: 'eventing-crds.yaml',
+    core_yaml: 'eventing-core.yaml',
+    version: '1.6.0',
+    manifestUrl: 'https://github.com/',
+};
+
+/**
+ * Implementation of KNative add-on for EKS Blueprints. Installs KNative to the Cluster.
+ */
+export class KNativeAddOn implements ClusterAddOn {
+    readonly knativeAddOnProps: KNativeProps;
+
+    deploy(clusterInfo: ClusterInfo): Promise<Construct> | void {
+        const cluster = clusterInfo.cluster;
+
+        let
+
+        const crds_manifest = loadExternalYaml();
+    }
+
+}
