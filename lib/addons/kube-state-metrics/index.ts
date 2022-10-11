a36a2b5c5d7c0a3662ff940d713023170eef6ef1
Elamaran Shanmugam <elamaran.shan@gmail.com>
2022-10-11 13:22:15 +0000

    Fixing Versions

--- lib/addons/kube-state-metrics/index.ts
+++ lib/addons/kube-state-metrics/index.ts
@@ -21,7 +21,7 @@ const defaultProps: HelmAddOnProps & KubeStateMetricsAddOnProps = {
   name: "kube-state-metrics",
   namespace: "kube-system",
   chart: "kube-state-metrics",
-  version: "4.16.0",
+  version: "4.20.2",
   release: "kube-state-metrics",
   repository:  "https://prometheus-community.github.io/helm-charts",
   values: {},
