80d3040bbc26c0ebfe5bdd32aefc5332759506e2
Shardul Vaidya <cam.v737@gmail.com>
2022-11-16 17:45:51 -0500

    Updated main for testing

--- bin/main.ts
+++ bin/main.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-
-import BlueprintConstruct from '../examples/blueprint-construct';
+import * as blueprints from '../lib';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { KubernetesVersion, NodegroupAmiType } from 'aws-cdk-lib/aws-eks';
 
 const app = new cdk.App();
 
@@ -9,4 +10,24 @@ const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = process.env.CDK_DEFAULT_REGION;
 const props = { env: { account, region } };
 
-new BlueprintConstruct(app, props);
+const addOns = [
+    new blueprints.addons.IstioBaseAddOn(),
+    new blueprints.addons.KNativeOperator()
+]
+
+
+const clusterProvider = new blueprints.GenericClusterProvider({
+    version: KubernetesVersion.V1_21,
+    managedNodeGroups: [
+        {
+            id: "mng1",
+            amiType: NodegroupAmiType.AL2_X86_64,
+            instanceTypes: [new ec2.InstanceType('m5.large')],
+        },
+    ]
+});
+
+blueprints.EksBlueprint.builder()
+    .addOns(...addOns)
+    .clusterProvider(clusterProvider)
+    .build(app, 'knative-blueprint-test', props);
