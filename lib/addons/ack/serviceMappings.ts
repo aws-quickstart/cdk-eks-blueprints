fc54b290ef0619bb44afccaaee754aa27693ea9d
Mikhail Shapirov <shapirov@amazon.com>
2022-12-06 17:47:22 -0500

    Improved design

--- lib/addons/ack/serviceMappings.ts
+++ lib/addons/ack/serviceMappings.ts
@@ -1,10 +1,20 @@
-export const serviceMappings = {
-    "IAM": {
+import { AckServiceName } from ".";
+
+export interface AckChartMapping {
+    chart: string,
+    version: string,
+    managedPolicyName: string
+}
+  
+export const serviceMappings : {
+    [key in AckServiceName]: AckChartMapping
+} = {
+    [AckServiceName.IAM]: {
       chart: "iam-chart",
       version:  "v0.0.13",
       managedPolicyName: "IAMFullAccess"
     },
-    "RDS": {
+    [AckServiceName.RDS]: {
       chart: "iam-chart",
       version:  "v0.0.13",
       managedPolicyName: "IAMFullAccess"
