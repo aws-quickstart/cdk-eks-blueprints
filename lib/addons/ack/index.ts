821ca7b305032a539b7e24f630ac2b9ef4a41957
Elamaran Shanmugam <elamaran.shan@gmail.com>
2022-12-06 20:29:26 +0000

    AWS Controller for K8s Addon

--- lib/addons/ack/index.ts
+++ lib/addons/ack/index.ts
@@ -12,6 +12,10 @@ import { serviceMappings } from "./serviceMappings";
  */
 export interface AckAddOnProps extends HelmAddOnUserProps {
     /**
+     * Required identified, must be unique within the parent stack scope.
+     */
+    id: string;
+    /**
      * Default Service Name
      * @default IAM
      */
@@ -31,6 +35,11 @@ export interface AckAddOnProps extends HelmAddOnUserProps {
     saName?: string;
 }
 
+export enum AckServiceName {
+  "IAM",
+  "RDS" // etc.
+}
+
 /**
  * Default props to be used when creating the Helm chart
  */
@@ -44,13 +53,12 @@ const defaultProps: AckAddOnProps = {
   values: {},
   managedPolicyName: "IAMFullAccess",
   createNamespace: true,
-  saName: "iam-chart"
+  saName: "iam-chart",
+  serviceName: AckServiceName.IAM, 
+  id: "IAM-ACK"
 };
 
-export enum AckServiceName {
-  "IAM",
-  "RDS" // etc.
-}
+
 
 /**
  * Main class to instantiate the Helm chart
@@ -58,26 +66,22 @@ export enum AckServiceName {
 export class AckAddOn extends HelmAddOn {
 
   readonly options: AckAddOnProps;
+  readonly id? : string;
 
   constructor(props?: AckAddOnProps) {
-    super({...defaultProps as any, ...props});
-    this.options = this.props as AckAddOnProps;
+    super(populateDefautls(defaultProps,props) as AckAddOnProps);
+    // this.options = this.props as AckAddOnProps;
   }
 
   deploy(clusterInfo: ClusterInfo): Promise<Construct> {
     const cluster = clusterInfo.cluster;
-    const saName = this.options.saName ?? `${this.options.chart}-sa`;
-    this.options.name = this.options.name ?? serviceMappings.IAM.chart;
-    this.options.chart = this.options.chart ?? serviceMappings.IAM.chart;
-    this.options.repository = this.options.repository ?? `oci://public.ecr.aws/aws-controllers-k8s\${this.options.chart}`;
-    
 
     const sa = cluster.addServiceAccount(`${this.options.chart}-sa`, {
       namespace: this.options.namespace,
-      name: saName,
+      name: this.options.saName,
     });
 
-    let values: Values = populateValues(this.options,cluster.stack.region,saName);
+    let values: Values = populateValues(this.options,cluster.stack.region);
     values = merge(values, this.props.values ?? {});
 
     if(this.options.createNamespace == true){
@@ -96,10 +100,28 @@ export class AckAddOn extends HelmAddOn {
  * populateValues populates the appropriate values used to customize the Helm chart
  * @param helmOptions User provided values to customize the chart
  */
-function populateValues(helmOptions: AckAddOnProps, awsRegion: string, saName: string): Values {
+function populateValues(helmOptions: AckAddOnProps, awsRegion: string): Values {
   const values = helmOptions.values ?? {};
   setPath(values, "aws.region", awsRegion);
   setPath(values,"serviceAccount.create", false);
-  setPath(values,"serviceAccount.name", saName);
+  setPath(values,"serviceAccount.name", helmOptions.saName);
   return values;
+}
+
+/**
+ * populate parameters passed or the default values from service Mappings.
+ */
+function populateDefautls(defaultProps: AckAddOnProps, props: AckAddOnProps): AckAddOnProps {
+  let tempProps : AckAddOnProps = {...props};
+  tempProps.id = props.id ?? defaultProps.id;
+  tempProps.serviceName = props.serviceName ?? defaultProps.serviceName;
+  tempProps.name = props.name ?? `serviceMappings.${tempProps.serviceName}.chart` ?? defaultProps.name;
+  tempProps.namespace = props.namespace ?? defaultProps.namespace;
+  tempProps.chart = props.chart ?? `serviceMappings.${tempProps.serviceName}.chart` ?? defaultProps.chart;
+  tempProps.version = props.chart ?? `serviceMappings.${tempProps.serviceName}.version` ?? defaultProps.version;
+  tempProps.repository = props.repository ?? `oci://public.ecr.aws/aws-controllers-k8s\${tempProps.name}` ?? defaultProps.repository;
+  tempProps.managedPolicyName = props.managedPolicyName ?? `serviceMappings.${tempProps.serviceName}.managedPolicyName` ?? defaultProps.managedPolicyName;
+  tempProps.createNamespace = props.createNamespace ?? defaultProps.createNamespace;
+  tempProps.saName = props.saName ?? `${props.chart}-sa`;
+  return tempProps;
 }
