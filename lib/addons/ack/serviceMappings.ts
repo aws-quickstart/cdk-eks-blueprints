92d85e27eab6765c25040829079bd1cc6f28eb98
Elamaran Shanmugam <elamaran.shan@gmail.com>
2022-12-07 02:22:43 +0000

    AWS Controller for K8s Addon

--- lib/addons/ack/serviceMappings.ts
+++ lib/addons/ack/serviceMappings.ts
@@ -7,17 +7,25 @@ export interface AckChartMapping {
 export const enum AckServiceName {
   IAM = "iam",
   RDS = "rds",
-  EC2 = "ec2"
+  EC2 = "ec2",
+  S3  = "s3",
+  DYNAMODB = "dynamodb",
+  ECR = "ecr",
+  SNS = "sns",
+  APIGATEWAY = "apigatewayv2",
+  ELASTICACHE = "elasticache",
+  OPENSEARCH = "opensearchservice",
+
 }
   
-export const serviceMappings : { [key in AckServiceName]?: AckChartMapping } = 
-{
-    "iam": {
+export const serviceMappings : {
+    [key in AckServiceName]?: AckChartMapping
+} = {
+    [AckServiceName.IAM]: {
       chart: "iam-chart",
       version:  "v0.0.13",
       managedPolicyName: "IAMFullAccess"
     },
-<<<<<<< HEAD
     [AckServiceName.RDS]: {
       chart: "rds-chart",
       version:  "v0.1.1",
@@ -27,11 +35,56 @@ export const serviceMappings : { [key in AckServiceName]?: AckChartMapping } =
       chart: "ec2-chart",
       version:  "v0.1.0",
       managedPolicyName: "AmazonRDSFullAccess"
-=======
-    "rds": {
-      chart: "iam-chart",
-      version:  "v0.0.13",
-      managedPolicyName: "IAMFullAccess"
->>>>>>> f36ce25720ae0689c3e329a489d5479d401de406
+    },
+    [AckServiceName.S3]: {
+      chart: "s3-chart",
+      version:  "v0.1.5",
+      managedPolicyName: "AmazonS3FullAccess"
+    },
+    [AckServiceName.DYNAMODB]: {
+      chart: "dynamodb-chart",
+      version:  "v0.1.7",
+      managedPolicyName: "AmazonDynamoDBFullAccess"
+    },
+    [AckServiceName.ECR]: {
+      chart: "ecr-chart",
+      version:  "v0.1.7",
+      managedPolicyName: "AmazonEC2ContainerRegistryFullAccess"
+    },
+    [AckServiceName.SNS]: {
+      chart: "sns-chart",
+      version:  "v0.0.1",
+      managedPolicyName: "AmazonSNSFullAccess"
+    },
+    [AckServiceName.APIGATEWAY]: {
+      chart: "apigatewayv2-chart",
+      version:  "v0.1.4",
+      managedPolicyName: ""
+    },
+    [AckServiceName.ELASTICACHE]: {
+      chart: "elasticache-chart",
+      version:  "v0.0.20",
+      managedPolicyName: ""
+    },
+    [AckServiceName.OPENSEARCH]: {
+      chart: "opensearchservice-chart",
+      version:  "v0.0.14",
+      managedPolicyName: ""
+    },
+    [AckServiceName.DYNAMODB]: {
+      chart: "dynamodb-chart",
+      version:  "v0.1.7",
+      managedPolicyName: "AmazonDynamoDBFullAccess"
+    },
+    [AckServiceName.ECR]: {
+      chart: "ecr-chart",
+      version:  "v0.1.7",
+      managedPolicyName: "AmazonEC2ContainerRegistryFullAccess"
+    },
+    [AckServiceName.SNS]: {
+      chart: "sns-chart",
+      version:  "v0.0.1",
+      managedPolicyName: "AmazonSNSFullAccess"
     }
-};
+
+}
