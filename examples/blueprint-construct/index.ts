71c07da2db7656cc1548434089d216ba63063e8c
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-02-25 02:48:12 +0000

    Adding Configuration values to VPC CNI Addon Types

--- examples/blueprint-construct/index.ts
+++ examples/blueprint-construct/index.ts
@@ -225,7 +225,7 @@ export default class BlueprintConstruct {
 
         blueprints.EksBlueprint.builder()
             .addOns(...addOns)
-            .resourceProvider(blueprints.GlobalResources.Vpc, new VpcProvider(undefined,"10.64.0.0/24",["10.64.0.0/26","10.64.0.32/26","10.64.0.64/26"],))
+            .resourceProvider(blueprints.GlobalResources.Vpc, new VpcProvider(undefined,"10.64.0.0/24",["10.64.0.0/25","10.64.0.32/26","10.64.0.64/26"],))
             .clusterProvider(clusterProvider)
             .teams(...teams, new blueprints.EmrEksTeam(dataTeam))
             .enableControlPlaneLogTypes(blueprints.ControlPlaneLogType.API)
