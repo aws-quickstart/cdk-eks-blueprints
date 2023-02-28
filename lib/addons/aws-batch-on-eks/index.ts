4dc5700aedd619424a5e260bc3fbbfbf959958a4
Young <youngjeong46@gmail.com>
2023-02-27 23:09:14 -0600

    flags for existing SLR

--- lib/addons/aws-batch-on-eks/index.ts
+++ lib/addons/aws-batch-on-eks/index.ts
@@ -9,22 +9,23 @@ export class AwsBatchAddOn implements ClusterAddOn {
   deploy(clusterInfo: ClusterInfo): Promise<Construct> {
     const cluster = clusterInfo.cluster;
 
-    // Create the service role used by AWS Batch on EKS 
-    const batchOnEksSlr = new CfnServiceLinkedRole(cluster.stack, 'BatchServiceRole', {
-      awsServiceName: 'batch.amazonaws.com',
-    });
+    // Create the service role used by AWS Batch on EKS if one doesn't exist
+    if (!Role.fromRoleName(cluster.stack, 'BatchServiceLinkedRole', 'AWSServiceRoleForBatch')){
+      new CfnServiceLinkedRole(cluster.stack, 'BatchServiceRole', {
+        awsServiceName: 'batch.amazonaws.com',
+      });
+    }    
 
     //Init the service role as IRole because `addRoleMapping` method does not
     //support the CfnServiceLinkedRole type
     const batchEksServiceRole: IRole = Role.fromRoleArn(
       cluster.stack,
       'ServiceRoleForBatch',
-      `arn:aws:iam::${Stack.of(cluster.stack).account
-      }:role/AWSServiceRoleForBatch`,
+      `arn:aws:iam::${Stack.of(cluster.stack).account}:role/AWSServiceRoleForBatch`,
     );
     
     //Add the service role to the AwsAuth
-    cluster.awsAuth.addRoleMapping(
+    const mappingRole = cluster.awsAuth.addRoleMapping(
       batchEksServiceRole,
       {
         username: BATCH,
@@ -32,7 +33,7 @@ export class AwsBatchAddOn implements ClusterAddOn {
       }
     );
   
-    return Promise.resolve(batchOnEksSlr);
+    return Promise.resolve(batchEksServiceRole);
 
   }
 }
