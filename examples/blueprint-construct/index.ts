2270586f8bd0575313567e10abfa939b459b05d5
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-03-01 19:56:29 +0000

    Fixing PR Issues 3

--- examples/blueprint-construct/index.ts
+++ examples/blueprint-construct/index.ts
@@ -258,7 +258,7 @@ export default class BlueprintConstruct {
 
         blueprints.EksBlueprint.builder()
             .addOns(...addOns)
-            .resourceProvider(blueprints.GlobalResources.Vpc, new VpcProvider(undefined,"10.64.0.0/24",["10.64.0.0/25","10.64.0.128/26","10.64.0.192/26"],))
+            .resourceProvider(blueprints.GlobalResources.Vpc, new VpcProvider(undefined,"100.64.0.0/10",["100.64.0.0/16","100.65.0.0/16","10.66.0.0/16"],))
             .clusterProvider(clusterProvider)
             .teams(...teams, new blueprints.EmrEksTeam(dataTeam), new blueprints.BatchEksTeam(batchTeam))
             .enableControlPlaneLogTypes(blueprints.ControlPlaneLogType.API)
