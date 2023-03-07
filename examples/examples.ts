0357cc5954cf3848e4b843cff1f0e3189a3c3505
shapirov103 <shapirov@amazon.com>
2023-03-07 00:35:02 -0500

    examples working

--- examples/examples.ts
+++ examples/examples.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as bp from '../lib';
 import * as bcrypt from 'bcrypt';
+import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
 
 const app = new cdk.App();
 
@@ -10,43 +12,55 @@ const base = bp.EksBlueprint.builder()
     .resourceProvider(bp.GlobalResources.Vpc, new bp.VpcProvider("default")); // saving time on VPC creation
 
 
-const builder = base.clone;
+const builder = () => base.clone();
+
+const publicCluster = {
+    version: KubernetesVersion.V1_24, 
+    vpcSubnets: [{ subnetType: ec2.SubnetType.PUBLIC }]
+};
 
 builder()
     .clusterProvider(new bp.FargateClusterProvider())
     .build(app, "fargate-blueprint");
 
 builder()
-    .clusterProvider(new bp.MngClusterProvider())
+    .clusterProvider(new bp.MngClusterProvider(publicCluster))
     .build(app, "mng-blueprint");
 
+builder()
+    .clusterProvider(new bp.MngClusterProvider(publicCluster))
+    .addOns(buildArgoBootstrap())
+    .build(app, 'argo-blueprint');
 
-const argoBootstrap = new bp.addons.ArgoCDAddOn({
-    bootstrapRepo: {
-         repoUrl: 'https://github.com/aws-samples/eks-blueprints-add-ons.git',
-         path: 'chart',
-         targetRevision: "eks-blueprints-cdk",
-    },
-    workloadApplications: [
-        {
-            name: "micro-services",
-            namespace: "argocd",
-            repository: {
-                repoUrl: 'https://github.com/aws-samples/eks-blueprints-workloads.git',
-                path: 'envs/dev',
-                targetRevision: "main",
-            },
-            values: {
-                domain: ""
+
+function buildArgoBootstrap() {
+    return new bp.addons.ArgoCDAddOn({
+        bootstrapRepo: {
+            repoUrl: 'https://github.com/aws-samples/eks-blueprints-add-ons.git',
+            path: 'chart',
+            targetRevision: "eks-blueprints-cdk",
+        },
+        workloadApplications: [
+            {
+                name: "micro-services",
+                namespace: "argocd",
+                repository: {
+                    repoUrl: 'https://github.com/aws-samples/eks-blueprints-workloads.git',
+                    path: 'envs/dev',
+                    targetRevision: "main",
+                },
+                values: {
+                    domain: ""
+                }
+            }
+        ],
+        values: {
+            configs: {
+                secret: {
+                    argocdServerAdminPassword: bcrypt.hash("argopwd1", 10)
+                }
             }
         }
-    ],
-    values: {
-        configs: {
-             secret: {
-                 argocdServerAdminPassword: bcrypt.hash("argopwd1", 10) 
-             }
-         }
-     }
-});
+    });
+}
 
