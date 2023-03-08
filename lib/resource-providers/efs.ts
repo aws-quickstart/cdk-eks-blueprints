25eb3de15afc1e49a4eb7f0f88a09ba37399b6ff
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-03-07 19:28:40 -0600

    Delegating encryption config to the pattern

--- lib/resource-providers/efs.ts
+++ lib/resource-providers/efs.ts
@@ -1,16 +1,16 @@
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as efs from 'aws-cdk-lib/aws-efs';
-import * as cdk from 'aws-cdk-lib/core';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as efs from "aws-cdk-lib/aws-efs";
+import * as cdk from "aws-cdk-lib/core";
 import { GlobalResources, ResourceContext, ResourceProvider } from "../spi";
 
 /**
  * Configuration options for the EFS file system.
  */
 export interface EfsFileSystemProps {
-    readonly name?: string;
-    readonly fileSystemId?: string;
-    readonly efsProps?: Omit<efs.FileSystemProps, "vpc">;
-    readonly removalPolicy?: cdk.RemovalPolicy;
+  readonly name?: string;
+  readonly fileSystemId?: string;
+  readonly efsProps?: Omit<efs.FileSystemProps, "vpc">;
+  readonly removalPolicy?: cdk.RemovalPolicy;
 }
 
 /**
@@ -22,64 +22,71 @@ export interface EfsFileSystemProps {
  * @param efsProps The props used for the file system.
  * @param removalPolicy The removal policy to use for the EFS file system.
  */
-export class EfsFileSystemProvider implements ResourceProvider<efs.IFileSystem> {
-    readonly options: EfsFileSystemProps;
+export class EfsFileSystemProvider
+  implements ResourceProvider<efs.IFileSystem>
+{
+  readonly options: EfsFileSystemProps;
 
-    constructor(options: EfsFileSystemProps) {
-        this.options = options;
+  constructor(options: EfsFileSystemProps) {
+    this.options = options;
+  }
+
+  provide(context: ResourceContext): efs.IFileSystem {
+    const id = context.scope.node.id;
+    const securityGroupId = `${id}-${
+      this.options.name ?? "default"
+    }-EfsSecurityGroup`;
+    let efsFileSystem: efs.IFileSystem | undefined;
+
+    if (this.options.fileSystemId && this.options.name) {
+      const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(
+        context.scope,
+        securityGroupId,
+        securityGroupId
+      );
+      efsFileSystem = efs.FileSystem.fromFileSystemAttributes(
+        context.scope,
+        this.options.name,
+        {
+          securityGroup: securityGroup,
+          fileSystemId: this.options.fileSystemId,
+        }
+      );
     }
 
-    provide(context: ResourceContext): efs.IFileSystem {
-        const id = context.scope.node.id;
-        const securityGroupId = `${id}-${this.options.name ?? 'default'}-EfsSecurityGroup`;
-        let efsFileSystem: efs.IFileSystem | undefined;
+    if (!efsFileSystem) {
+      const vpc = context.get(GlobalResources.Vpc) as ec2.IVpc;
+      if (vpc === undefined) {
+        throw new Error("VPC not found in context");
+      }
+      const removalPolicy = this.options.removalPolicy ?? context.removalPolicy;
+      const clusterVpcCidr = vpc.vpcCidrBlock;
 
-        if (this.options.fileSystemId && this.options.name) {
-            const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(
-                context.scope,
-                securityGroupId,
-                securityGroupId,
-            );
-            efsFileSystem = efs.FileSystem.fromFileSystemAttributes(
-                context.scope, this.options.name,
-                {
-                    securityGroup: securityGroup,
-                    fileSystemId: this.options.fileSystemId,
-                }
-            );
+      const efsSG = new ec2.SecurityGroup(context.scope, securityGroupId, {
+        vpc,
+        securityGroupName: securityGroupId,
+      });
+      efsSG.addIngressRule(
+        ec2.Peer.ipv4(clusterVpcCidr),
+        new ec2.Port({
+          protocol: ec2.Protocol.TCP,
+          stringRepresentation: "EFSconnection",
+          toPort: 2049,
+          fromPort: 2049,
+        })
+      );
+
+      efsFileSystem = new efs.FileSystem(
+        context.scope,
+        this.options.name || `${id}-EfsFileSystem`,
+        {
+          vpc,
+          securityGroup: efsSG,
+          removalPolicy,
+          ...this.options.efsProps,
         }
-
-        if (!efsFileSystem) {
-            const vpc = context.get(GlobalResources.Vpc) as ec2.IVpc;
-
-            if (vpc === undefined) {
-                throw new Error('VPC not found in context');
-            }
-            const removalPolicy = this.options.removalPolicy ?? context.removalPolicy;
-            const clusterVpcCidr = vpc.vpcCidrBlock;
-
-            const efsSG = new ec2.SecurityGroup(context.scope, securityGroupId, { vpc, securityGroupName: securityGroupId, });
-            efsSG.addIngressRule(
-                ec2.Peer.ipv4(clusterVpcCidr),
-                new ec2.Port({
-                    protocol: ec2.Protocol.TCP,
-                    stringRepresentation: "EFSconnection",
-                    toPort: 2049,
-                    fromPort: 2049,
-                }),
-            );
-
-            efsFileSystem = new efs.FileSystem(
-                context.scope, this.options.name || `${id}-EfsFileSystem`,
-                {
-                    vpc,
-                    securityGroup: efsSG,
-                    removalPolicy,
-                    encrypted: true,
-                    ...this.options.efsProps,
-                }
-            );
-        }
-        return efsFileSystem;
+      );
     }
+    return efsFileSystem;
+  }
 }
