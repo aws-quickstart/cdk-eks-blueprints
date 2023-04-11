88bc5a52ee03aed378922b2944b61e70e87d690d
Aliaksei Ivanou <coloboslexx@gmail.com>
2023-04-11 12:18:12 -0400

    Adding backward compatibility with v1.6.0 for the KMS key resource provider

--- lib/resource-providers/kms-key.ts
+++ lib/resource-providers/kms-key.ts
@@ -15,22 +15,29 @@ import { ResourceContext, ResourceProvider } from "../spi";
  */
 export class CreateKmsKeyProvider implements ResourceProvider<kms.IKey> {
   private readonly aliasName?: string;
+  private readonly keyId?: string;
   private readonly kmsKeyProps?: kms.KeyProps;
 
   /**
    * Configuration options for the KMS Key.
    *
    * @param aliasName The alias name for the KMS Key
+   * @param keyId The key id for the KMS Key
    * @param kmsKeyProps The key props used
    */
-  public constructor(aliasName?: string, kmsKeyProps?: kms.KeyProps) {
+  public constructor(
+    aliasName?: string,
+    keyId?: string,
+    kmsKeyProps?: kms.KeyProps
+  ) {
     this.aliasName = aliasName;
+    this.keyId = keyId;
     this.kmsKeyProps = kmsKeyProps;
   }
 
   provide(context: ResourceContext): kms.IKey {
     const id = context.scope.node.id;
-    const keyId = `${id}-${this.aliasName ?? "default"}-KmsKey`;
+    const keyId = this.keyId || `${id}-${this.aliasName ?? "default"}-KmsKey`;
     let key = undefined;
 
     key = new kms.Key(context.scope, keyId, {
