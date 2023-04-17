90ba0b194fb88099f62db72a65af430c1ec813bd
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-04-17 19:25:12 +0000

    Fixing PR

--- examples/blueprint-construct/index.ts
+++ examples/blueprint-construct/index.ts
@@ -171,8 +171,7 @@ export default class BlueprintConstruct {
             new blueprints.AwsBatchAddOn(),
             new blueprints.UpboundUniversalCrossplaneAddOn(),
             new blueprints.AwsForFluentBitAddOn(),
-            new blueprints.FluxCDAddOn({
-            }),
+            new blueprints.FluxCDAddOn(),
         ];
 
         // Instantiated to for helm version check.
