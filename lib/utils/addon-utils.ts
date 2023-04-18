def668ee73b7e43a425032ed16c7f831ac2d1268
Shardul Vaidya <vshardul@amazon.com>
2023-04-18 12:10:24 -0400

    Reverted broad reformatting performed by WebStorm

--- lib/utils/addon-utils.ts
+++ lib/utils/addon-utils.ts
@@ -1,5 +1,5 @@
 import * as assert from "assert";
-import {Construct} from "constructs";
+import { Construct } from "constructs";
 import "reflect-metadata";
 import {ClusterAddOn, ClusterInfo, Values} from '../spi';
 import {escapeDots} from "./string-utils";
@@ -13,30 +13,29 @@ export function getAddOnNameOrId(addOn: ClusterAddOn): string {
   return addOn.id ?? addOn.constructor.name;
 }
 
-export function isOrderedAddOn(addOn: ClusterAddOn): boolean {
-  return Reflect.getMetadata("ordered", addOn.constructor) ?? Reflect.getMetadata("ordered", addOn) ?? false;
+export function isOrderedAddOn(addOn: ClusterAddOn) : boolean {
+    return Reflect.getMetadata("ordered", addOn.constructor) ?? Reflect.getMetadata("ordered", addOn) ?? false;
 }
-
 /**
  * Decorator function that accepts a list of AddOns and
  * ensures addons are scheduled to be added as well as
  * add them as dependencies
- * @param addOns
- * @returns
+ * @param addOns 
+ * @returns 
  */
 export function dependable(...addOns: string[]) {
   // eslint-disable-next-line @typescript-eslint/ban-types
   return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
 
-    descriptor.value = function (...args: any[]) {
+    descriptor.value = function( ...args: any[]) {
       const dependencies = Array<Promise<Construct>>();
       const clusterInfo: ClusterInfo = args[0];
       const stack = clusterInfo.cluster.stack.stackName;
 
-      addOns.forEach((addOn) => {
+      addOns.forEach( (addOn) => {
         const dep = clusterInfo.getScheduledAddOn(addOn);
-        assert(dep, `Missing a dependency for ${addOn} for ${stack}`);
+        assert(dep, `Missing a dependency for ${addOn} for ${stack}`); 
         dependencies.push(dep!);
       });
 
@@ -44,13 +43,11 @@ export function dependable(...addOns: string[]) {
 
       Promise.all(dependencies.values()).then((constructs) => {
         constructs.forEach((construct) => {
-          result.then((resource) => {
-            resource.node.addDependency(construct);
-          });
+            result.then((resource) => {
+              resource.node.addDependency(construct);
+            });
         });
-      }).catch(err => {
-        throw new Error(err);
-      });
+      }).catch(err => { throw new Error(err); });
 
       return result;
     };
@@ -63,22 +60,22 @@ export function dependable(...addOns: string[]) {
  * Decorator function that accepts a list of AddOns and
  * throws error if those addons are scheduled to be added as well
  * As they should not be deployed with
- * @param addOns
- * @returns
+ * @param addOns 
+ * @returns 
  */
 export function conflictsWith(...addOns: string[]) {
   // eslint-disable-next-line @typescript-eslint/ban-types
   return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
 
-    descriptor.value = function (...args: any[]) {
+    descriptor.value = function( ...args: any[]) {
       // const dependencies: (Promise<Construct> | undefined)[] = [];
       const clusterInfo: ClusterInfo = args[0];
       const stack = clusterInfo.cluster.stack.stackName;
 
-      addOns.forEach((addOn) => {
+      addOns.forEach( (addOn) => {
         const dep = clusterInfo.getScheduledAddOn(addOn);
-        if (dep) {
+        if (dep){
           throw new Error(`Deploying ${stack} failed due to conflicting add-on: ${addOn}.`);
         }
       });
