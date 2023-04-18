4adc7d7315f09b33987f371e887e445a8ae0c083
Shardul Vaidya <vshardul@amazon.com>
2023-04-18 10:02:49 -0400

    Moved common functions to addon-utils.ts
    Added RDS to spi/types.ts

--- lib/utils/addon-utils.ts
+++ lib/utils/addon-utils.ts
@@ -1,7 +1,8 @@
 import * as assert from "assert";
-import { Construct } from "constructs";
+import {Construct} from "constructs";
 import "reflect-metadata";
-import { ClusterAddOn, ClusterInfo } from '../spi';
+import {ClusterAddOn, ClusterInfo, Values} from '../spi';
+import {escapeDots} from "./string-utils";
 
 /**
  * Returns AddOn Id if defined else returns the class name
@@ -12,29 +13,30 @@ export function getAddOnNameOrId(addOn: ClusterAddOn): string {
   return addOn.id ?? addOn.constructor.name;
 }
 
-export function isOrderedAddOn(addOn: ClusterAddOn) : boolean {
-    return Reflect.getMetadata("ordered", addOn.constructor) ?? Reflect.getMetadata("ordered", addOn) ?? false;
+export function isOrderedAddOn(addOn: ClusterAddOn): boolean {
+  return Reflect.getMetadata("ordered", addOn.constructor) ?? Reflect.getMetadata("ordered", addOn) ?? false;
 }
+
 /**
  * Decorator function that accepts a list of AddOns and
  * ensures addons are scheduled to be added as well as
  * add them as dependencies
- * @param addOns 
- * @returns 
+ * @param addOns
+ * @returns
  */
 export function dependable(...addOns: string[]) {
   // eslint-disable-next-line @typescript-eslint/ban-types
   return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
 
-    descriptor.value = function( ...args: any[]) {
+    descriptor.value = function (...args: any[]) {
       const dependencies = Array<Promise<Construct>>();
       const clusterInfo: ClusterInfo = args[0];
       const stack = clusterInfo.cluster.stack.stackName;
 
-      addOns.forEach( (addOn) => {
+      addOns.forEach((addOn) => {
         const dep = clusterInfo.getScheduledAddOn(addOn);
-        assert(dep, `Missing a dependency for ${addOn} for ${stack}`); 
+        assert(dep, `Missing a dependency for ${addOn} for ${stack}`);
         dependencies.push(dep!);
       });
 
@@ -42,11 +44,13 @@ export function dependable(...addOns: string[]) {
 
       Promise.all(dependencies.values()).then((constructs) => {
         constructs.forEach((construct) => {
-            result.then((resource) => {
-              resource.node.addDependency(construct);
-            });
+          result.then((resource) => {
+            resource.node.addDependency(construct);
+          });
         });
-      }).catch(err => { throw new Error(err); });
+      }).catch(err => {
+        throw new Error(err);
+      });
 
       return result;
     };
@@ -59,22 +63,22 @@ export function dependable(...addOns: string[]) {
  * Decorator function that accepts a list of AddOns and
  * throws error if those addons are scheduled to be added as well
  * As they should not be deployed with
- * @param addOns 
- * @returns 
+ * @param addOns
+ * @returns
  */
 export function conflictsWith(...addOns: string[]) {
   // eslint-disable-next-line @typescript-eslint/ban-types
   return function (target: Object, key: string | symbol, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
 
-    descriptor.value = function( ...args: any[]) {
+    descriptor.value = function (...args: any[]) {
       // const dependencies: (Promise<Construct> | undefined)[] = [];
       const clusterInfo: ClusterInfo = args[0];
       const stack = clusterInfo.cluster.stack.stackName;
 
-      addOns.forEach( (addOn) => {
+      addOns.forEach((addOn) => {
         const dep = clusterInfo.getScheduledAddOn(addOn);
-        if (dep){
+        if (dep) {
           throw new Error(`Deploying ${stack} failed due to conflicting add-on: ${addOn}.`);
         }
       });
@@ -84,4 +88,26 @@ export function conflictsWith(...addOns: string[]) {
 
     return descriptor;
   };
+}
+
+/**
+ * Iterate an object to normalize the string format before sending to helm.
+ * For example, escaping the dot from certain keys: "ingress.annotations.kubernetes\\.io/tls-acme"
+ * @returns Values
+ * @param obj: Values
+ */
+export function normalizeValues(obj: Values): Values {
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      obj[key] = normalizeValues(obj[key]);
+    }
+
+    const escapedKey = escapeDots(key);
+    if (escapedKey != key) {
+      obj[escapedKey] = obj[key];
+      delete obj[key];
+    }
+  });
+
+  return obj;
 }
