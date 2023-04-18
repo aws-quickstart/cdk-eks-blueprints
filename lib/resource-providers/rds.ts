c17e0362679376e0cdfa4a4a6e6f19d6b1ffaec4
Shardul Vaidya <vshardul@amazon.com>
2023-04-18 10:38:23 -0400

    Fixed conflicts with default VPC

--- lib/resource-providers/rds.ts
+++ lib/resource-providers/rds.ts
@@ -1,10 +1,56 @@
 import { CfnOutput } from "aws-cdk-lib";
-import * as aurora from "aws-cdk-lib/"
+import * as rds from "aws-cdk-lib/aws-rds";
 import { GlobalResources, ResourceContext, ResourceProvider } from "../spi";
+import {IClusterEngine, IDatabaseCluster, InstanceProps} from "aws-cdk-lib/aws-rds";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as kms from "aws-cdk-lib/aws-kms";
+import {IVpc} from "aws-cdk-lib/aws-ec2";
 
-/**
- * Aurora resource provider.
- *
- * @param name The name of the
- */
-export class CreateAuroraProvider implements ResourceProvider<>
+
+export interface RdsInstanceProps {
+  readonly name?: string;
+  readonly rdsProps?: Omit<rds.DatabaseInstanceProps, "storageEncryptionKey">;
+  readonly rdsEncryptionKeyResourceName?: string;
+}
+
+export interface AuroraClusterProps {
+  readonly name?: string;
+  readonly clusterProps?: Omit<rds.DatabaseClusterProps, "engine" | "instanceProps">
+  readonly instanceProps?: InstanceProps;
+  readonly auroraEngine: IClusterEngine;
+}
+
+export class AuroraClusterProvider
+  implements ResourceProvider<rds.IDatabaseCluster> {
+  readonly options: AuroraClusterProps;
+
+  constructor(options: AuroraClusterProps) {
+    this.options = options;
+  }
+
+  provide(context: ResourceContext): IDatabaseCluster {
+    const id = context.scope.node.id;
+
+    const instanceProps: InstanceProps = this.options.instanceProps ?? {
+      vpc: context.get(GlobalResources.Vpc) as IVpc ?? new ec2.Vpc(
+        context.scope,
+        `${this.options.name}-${id}-Vpc`
+      ),
+    };
+
+    let auroraInstance = new rds.DatabaseCluster(
+      context.scope,
+      this.options.name || `${id}-AuroraInstance`,
+      {
+        engine: this.options.auroraEngine,
+        instanceProps: instanceProps
+      }
+    );
+
+    new CfnOutput(context.scope, "AuroraInstanceId", {
+      value: auroraInstance.clusterIdentifier
+    });
+
+    return auroraInstance;
+  }
+}
