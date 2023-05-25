b82bcafe97477015a65a97b3efed5419153145a9
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-05-25 14:40:09 -0400

    Flux and AMG

--- lib/pipelines/code-pipeline.ts
+++ lib/pipelines/code-pipeline.ts
@@ -89,28 +89,29 @@ export function isCodeStarConnection(
  * Props for the Pipeline.
  */
 export type PipelineProps = {
-  /**
-   * The name for the pipeline.
-   */
-  name: string;
 
-  /**
-   * The owner of the repository for the pipeline (GitHub handle).
-   */
-  owner?: string;
+    /**
+     * The name for the pipeline.
+     */
+    name: string;
 
-  /**
-   * Enable/Disable allowing cross-account deployments.
-   */
-  crossAccountKeys: boolean;
+    /**
+     * The owner of the repository for the pipeline (GitHub handle).
+     */
+    owner?: string;
 
-  /**
-   * IAM policies to attach to the code build role.
-   * By default it allows access for lookups, including secret look-ups.
-   * Passing an empty list will result in no extra-policies passed to the build role.
-   * Leaving this unspecified will result in the default policy applied (not recommended for proudction).
-   */
-  codeBuildPolicies?: PolicyStatement[];
+    /**
+     * Enable/Disable allowing cross-account deployments.
+     */
+    crossAccountKeys: boolean;
+
+    /**
+     * IAM policies to attach to the code build role. 
+     * By default it allows access for lookups, including secret look-ups.
+     * Passing an empty list will result in no extra-policies passed to the build role.
+     * Leaving this unspecified will result in the default policy applied (not recommended for proudction).
+     */
+    codeBuildPolicies?: PolicyStatement[];
 
   /**
    * Repository for the pipeline (GitHub or CodeCommitRepository).
@@ -120,111 +121,111 @@ export type PipelineProps = {
     | CodeCommitSourceRepository
     | CodeStarConnectionRepository;
 
-  /**
-   * Pipeline stages and options.
-   */
-  stages: WaveStage[];
+    /**
+     * Pipeline stages and options.
+     */
+    stages: WaveStage[];
 
-  /**
-   * Waves for the pipeline. Stages inside the wave are executed in parallel.
-   */
-  waves: PipelineWave[];
-};
+    /**
+     * Waves for the pipeline. Stages inside the wave are executed in parallel.
+     */
+    waves: PipelineWave[];
+}
 
 /**
  * Stack stage is a builder construct to allow adding stages to a pipeline. Each stage is expected to produce a stack.
  */
 export interface StackStage {
-  /**
-   * id of the stage
-   */
-  id: string;
+    /**
+     * id of the stage
+     */
+    id: string;
 
-  /**
-   * Builder that can produce a stack which will be deployed as part of the stage
-   */
-  stackBuilder: StackBuilder;
+    /**
+     * Builder that can produce a stack which will be deployed as part of the stage
+     */
+    stackBuilder: StackBuilder;
 
-  /**
-   * Optional stage properties, such as {manualApprovals: true} which can control stage transitions.
-   */
-  stageProps?: cdkpipelines.AddStageOpts;
+    /**
+     * Optional stage properties, such as {manualApprovals: true} which can control stage transitions.
+     */
+    stageProps?: cdkpipelines.AddStageOpts;
 }
 
 /**
  * Internal interface for wave stages
  */
 interface WaveStage extends StackStage {
-  /**
-   * Wave id if this stage is part of a wave. Not required if stage is supplied
-   */
-  waveId?: string;
+    /**
+     * Wave id if this stage is part of a wave. Not required if stage is supplied
+     */
+    waveId?: string,
 }
 
 /**
  * Represents wave configuration
  */
 export interface PipelineWave {
-  id: string;
 
-  stages: StackStage[];
+    id: string,
 
-  props?: cdkpipelines.WaveProps;
+    stages: StackStage[],
+
+    props?: cdkpipelines.WaveProps
 }
 
 /**
- * Default policy for the CodeBuild role generated.
- * It allows look-ups, including access to AWS Secrets Manager.
- * Not recommended for production. For production use case, CodeBuild policies
- * must be restricted to particular resources. Outbound access from the build should be
+ * Default policy for the CodeBuild role generated. 
+ * It allows look-ups, including access to AWS Secrets Manager. 
+ * Not recommended for production. For production use case, CodeBuild policies 
+ * must be restricted to particular resources. Outbound access from the build should be 
  * controlled by ACL.
  */
-export const DEFAULT_BUILD_POLICIES = [
-  new PolicyStatement({
-    resources: ['*'],
-    actions: [
-      'sts:AssumeRole',
-      'secretsmanager:GetSecretValue',
-      'secretsmanager:DescribeSecret',
-      'cloudformation:*',
-    ],
-  }),
-];
+export const DEFAULT_BUILD_POLICIES = [ new PolicyStatement({
+    resources: ["*"],
+    actions: [    
+        "sts:AssumeRole",
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:DescribeSecret",
+        "cloudformation:*"
+    ]
+})];
 
 /**
  * Builder for CodePipeline.
  */
 export class CodePipelineBuilder implements StackBuilder {
-  private props: Partial<PipelineProps>;
 
-  constructor() {
-    this.props = { crossAccountKeys: false, stages: [], waves: [] };
-  }
+    private props: Partial<PipelineProps>;
 
-  public name(name: string): CodePipelineBuilder {
-    this.props.name = name;
-    return this;
-  }
+    constructor() {
+        this.props = { crossAccountKeys: false, stages: [], waves: []};
+    }
 
-  public owner(owner: string): CodePipelineBuilder {
-    this.props.owner = owner;
-    return this;
-  }
+    public name(name: string): CodePipelineBuilder {
+        this.props.name = name;
+        return this;
+    }
 
-  /**
-   * For production use cases, make sure all policies are tied to concrete resources.
-   * @param policies
-   * @returns
-   */
-  public codeBuildPolicies(policies: PolicyStatement[]): CodePipelineBuilder {
-    this.props.codeBuildPolicies = policies;
-    return this;
-  }
+    public owner(owner: string) : CodePipelineBuilder {
+        this.props.owner = owner;
+        return this;
+    }
 
-  public enableCrossAccountKeys(): CodePipelineBuilder {
-    this.props.crossAccountKeys = true;
-    return this;
-  }
+    /**
+     * For production use cases, make sure all policies are tied to concrete resources.
+     * @param policies 
+     * @returns 
+     */
+    public codeBuildPolicies(policies: PolicyStatement[]) : CodePipelineBuilder {
+        this.props.codeBuildPolicies = policies;
+        return this;
+    }
+ 
+    public enableCrossAccountKeys() : CodePipelineBuilder {
+        this.props.crossAccountKeys = true;
+        return this;
+    }
 
   public repository(
     repo:
@@ -242,30 +243,28 @@ export class CodePipelineBuilder implements StackBuilder {
     return this;
   }
 
-  /**
-   * Adds standalone pipeline stages (in the order of invocation and elements in the input array)
-   * @param stackStages
-   * @returns
-   */
-  public stage(...stackStages: StackStage[]): CodePipelineBuilder {
-    stackStages.forEach((stage) => this.props.stages!.push(stage));
-    return this;
-  }
+    /**
+     * Adds standalone pipeline stages (in the order of invocation and elements in the input array)
+     * @param stackStages
+     * @returns
+     */
+    public stage(...stackStages: StackStage[]) : CodePipelineBuilder {
+        stackStages.forEach(stage => this.props.stages!.push(stage));
+        return this;
+    }
 
-  /**
-   * Adds wave(s) in the order specified. All stages in the wave can be executed in parallel, while standalone stages are executed sequentially.
-   * @param waves
-   * @returns
-   */
-  public wave(...waves: PipelineWave[]): CodePipelineBuilder {
-    waves.forEach((wave) => {
-      this.props.waves!.push(wave);
-      wave.stages.forEach((stage) =>
-        this.props.stages?.push({ ...stage, ...{ waveId: wave.id } })
-      );
-    });
-    return this;
-  }
+    /**
+     * Adds wave(s) in the order specified. All stages in the wave can be executed in parallel, while standalone stages are executed sequentially.
+     * @param waves
+     * @returns
+     */
+    public wave(...waves: PipelineWave[]) : CodePipelineBuilder {
+        waves.forEach(wave => {
+            this.props.waves!.push(wave);
+            wave.stages.forEach(stage => this.props.stages?.push({...stage, ...{ waveId: wave.id}}));
+        });
+        return this;
+    }
 
   build(scope: Construct, id: string, stackProps?: cdk.StackProps): cdk.Stack {
     assert(
@@ -290,111 +289,89 @@ export class CodePipelineBuilder implements StackBuilder {
   }
 }
 
+
 /**
  * Pipeline stack is generating a self-mutating pipeline to faciliate full CI/CD experience with the platform
  * for infrastructure changes.
  */
 export class CodePipelineStack extends cdk.Stack {
-  static readonly USAGE_ID = 'qs-1s1r465k6';
-  static readonly USAGE_ID_MULTI_ACCOUNT = 'qs-1s1r465f2';
 
-  static builder() {
-    return new CodePipelineBuilder();
-  }
+    static readonly USAGE_ID = "qs-1s1r465k6";
+    static readonly USAGE_ID_MULTI_ACCOUNT = "qs-1s1r465f2";
 
-  constructor(
-    scope: Construct,
-    pipelineProps: PipelineProps,
-    id: string,
-    props?: StackProps
-  ) {
-    if (pipelineProps.crossAccountKeys) {
-      super(
-        scope,
-        id,
-        withUsageTracking(CodePipelineStack.USAGE_ID_MULTI_ACCOUNT, props)
-      );
-    } else {
-      super(scope, id, withUsageTracking(CodePipelineStack.USAGE_ID, props));
+    static builder() {
+        return new CodePipelineBuilder();
     }
 
-    const pipeline = CodePipeline.build(this, pipelineProps);
-
-    let promises: Promise<ApplicationStage>[] = [];
-
-    for (let stage of pipelineProps.stages) {
-      const appStage = new ApplicationStage(this, stage.id, stage.stackBuilder);
-      promises.push(appStage.waitForAsyncTasks());
-    }
-
-    Promise.all(promises).then((stages) => {
-      let currentWave: cdkpipelines.Wave | undefined;
-
-      for (let i in stages) {
-        const stage = pipelineProps.stages[i];
-        if (stage.waveId) {
-          if (currentWave == null || currentWave.id != stage.waveId) {
-            const waveProps = pipelineProps.waves.find(
-              (wave) => wave.id === stage.waveId
-            );
-            assert(
-              waveProps,
-              `Specified wave ${stage.waveId} is not found in the pipeline definition ${id}`
-            );
-            currentWave = pipeline.addWave(stage.waveId, {
-              ...waveProps.props,
-            });
-          }
-          currentWave.addStage(stages[i], stage.stageProps);
+    constructor(scope: Construct, pipelineProps: PipelineProps, id: string,  props?: StackProps) {
+        if (pipelineProps.crossAccountKeys){
+            super(scope, id, withUsageTracking(CodePipelineStack.USAGE_ID_MULTI_ACCOUNT, props));
         } else {
-          pipeline.addStage(stages[i], stage.stageProps);
+            super(scope, id, withUsageTracking(CodePipelineStack.USAGE_ID, props));
         }
-      }
-    });
-  }
+
+        const pipeline = CodePipeline.build(this, pipelineProps);
+
+        let promises : Promise<ApplicationStage>[] = [];
+
+        for(let stage of pipelineProps.stages) {
+            const appStage = new ApplicationStage(this, stage.id, stage.stackBuilder);
+            promises.push(appStage.waitForAsyncTasks());
+        }
+
+        Promise.all(promises).then(stages => {
+            let currentWave : cdkpipelines.Wave | undefined;
+
+            for(let i in stages) {
+                const stage = pipelineProps.stages[i];
+                if(stage.waveId) {
+                    if(currentWave == null || currentWave.id != stage.waveId) {
+                        const waveProps = pipelineProps.waves.find(wave => wave.id === stage.waveId);
+                        assert(waveProps, `Specified wave ${stage.waveId} is not found in the pipeline definition ${id}`);
+                        currentWave = pipeline.addWave(stage.waveId, { ...waveProps.props });
+                    }
+                    currentWave.addStage(stages[i], stage.stageProps);
+                }
+                else {
+                    pipeline.addStage(stages[i], stage.stageProps);
+                }
+            }
+        });
+    }
 }
 
+
 export class ApplicationStage extends cdk.Stage {
-  private asyncTask: Promise<any>;
 
-  constructor(
-    scope: cdk.Stack,
-    id: string,
-    builder: StackBuilder | AsyncStackBuilder,
-    props?: cdk.StageProps
-  ) {
-    super(scope, id, props);
-    if ((<AsyncStackBuilder>builder).buildAsync !== undefined) {
-      this.asyncTask = (<AsyncStackBuilder>builder).buildAsync(
-        this,
-        `${id}-blueprint`,
-        props
-      );
-    } else {
-      builder.build(this, `${id}-blueprint`, props);
-    }
-  }
+    private asyncTask: Promise<any>;
 
-  public async waitForAsyncTasks(): Promise<ApplicationStage> {
-    if (this.asyncTask) {
-      return this.asyncTask.then(() => {
-        return this;
-      });
+    constructor(scope: cdk.Stack, id: string, builder: StackBuilder | AsyncStackBuilder, props?: cdk.StageProps) {
+        super(scope, id, props);
+        if((<AsyncStackBuilder>builder).buildAsync !== undefined) {
+            this.asyncTask = (<AsyncStackBuilder>builder).buildAsync(this, `${id}-blueprint`, props);
+        }
+        else {
+            builder.build(this, `${id}-blueprint`, props);
+        }
     }
-    return Promise.resolve(this);
-  }
+
+    public async waitForAsyncTasks() : Promise<ApplicationStage> {
+        if(this.asyncTask) {
+            return this.asyncTask.then(()=> {
+                return this;
+            });
+        }
+        return Promise.resolve(this);
+    }
 }
 
 /**
  * CodePipeline deploys a new CodePipeline resource that is integrated with a GitHub repository.
  */
 class CodePipeline {
-  public static build(
-    scope: Construct,
-    props: PipelineProps
-  ): cdkpipelines.CodePipeline {
-    let codePipelineSource: cdkpipelines.CodePipelineSource | undefined =
-      undefined;
+
+    public static build(scope: Construct, props: PipelineProps) : cdkpipelines.CodePipeline {
+        let codePipelineSource : cdkpipelines.CodePipelineSource | undefined = undefined;
 
         switch (true) {
           case isCodeCommitRepo(props.repository):{
@@ -444,21 +421,21 @@ class CodePipeline {
           }
         }
 
-    return new cdkpipelines.CodePipeline(scope, props.name, {
-      pipelineName: props.name,
-      synth: new cdkpipelines.ShellStep(`${props.name}-synth`, {
-        input: codePipelineSource,
-        installCommands: [
-          'n stable',
-          'npm install -g aws-cdk@2.66.1',
-          'npm install',
-        ],
-        commands: ['npm run build', 'npx cdk synth'],
-      }),
-      crossAccountKeys: props.crossAccountKeys,
-      codeBuildDefaults: {
-        rolePolicy: props.codeBuildPolicies,
-      },
-    });
-  }
+        return new cdkpipelines.CodePipeline(scope, props.name, {
+            pipelineName: props.name,
+            synth: new cdkpipelines.ShellStep(`${props.name}-synth`, {
+              input: codePipelineSource,
+              installCommands: [
+                'n stable',
+                'npm install -g aws-cdk@2.78.0',
+                'npm install',
+              ],
+              commands: ['npm run build', 'npx cdk synth']
+            }),
+            crossAccountKeys: props.crossAccountKeys,
+            codeBuildDefaults: {
+                rolePolicy: props.codeBuildPolicies
+            }
+          });
+    }
 }
