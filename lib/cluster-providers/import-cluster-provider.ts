5688a278784b2121ae2ca306f4ac77ec67d11453
shapirov <shapirov@amazon.com>
2023-06-27 00:51:18 -0400

    import cluster provider that supports auto configuration based on the eks API call to DescribeCluster

--- lib/cluster-providers/import-cluster-provider.ts
+++ lib/cluster-providers/import-cluster-provider.ts
@@ -1,8 +1,14 @@
-import { ClusterInfo, ClusterProvider, selectKubectlLayer } from "../";
+import { ClusterInfo, ClusterProvider } from "../spi";
+import { selectKubectlLayer } from "./generic-cluster-provider";
 import { IVpc } from "aws-cdk-lib/aws-ec2";
 import * as eks from "aws-cdk-lib/aws-eks";
+import { Role } from "aws-cdk-lib/aws-iam";
 import { IKey } from "aws-cdk-lib/aws-kms";
+import * as sdk from "@aws-sdk/client-eks";
 import { Construct } from "constructs";
+import { getResource } from "../resource-providers/utils";
+import { LookupOpenIdConnectProvider, LookupRoleProvider } from "../resource-providers";
+import { logger } from "../utils";
 
 
 /**
@@ -39,5 +45,56 @@ export class ImportClusterProvider implements ClusterProvider {
         
         const existingCluster = eks.Cluster.fromClusterAttributes(scope, 'imported-cluster-' + this.props.clusterName, props);
         return new ClusterInfo(existingCluster, this.props.version);
+    }
+
+
+    /**
+     * Requires iam permission to eks.DescribeCluster at build time. Retrieves the cluster information using DescribeCluster api and
+     * creates an import cluster provider. 
+     * @param clusterName name of the cluster
+     * @param region target rego
+     * @param version version of the cluster to be imported
+     * @param kubectlRole iam Role that provides access to the cluster API (kubectl). The CDK custom resource should be able to assume the role
+     * which in some cases may require trust policy for the account root principal.
+     * @returns the cluster provider with the import cluster configuration
+     */
+    public static async fromClusterLookup(clusterName: string, region: string, version: eks.KubernetesVersion, kubectlRole: Role) : 
+        Promise<ClusterProvider> {
+
+        const sdkCluster = await getCluster(clusterName, process.env.CDK_DEFAULT_REGION!);
+
+        return new ImportClusterProvider({
+            clusterName,
+            version: version,
+            clusterEndpoint: sdkCluster.endpoint,
+            openIdConnectProvider: getResource(context =>
+                new LookupOpenIdConnectProvider(sdkCluster.identity!.oidc!.issuer!).provide(context)),
+            clusterCertificateAuthorityData: sdkCluster.certificateAuthority?.data,
+            kubectlRoleArn: getResource(context =>
+                new LookupRoleProvider('awsqs-kubernetes-helm').provide(context)).roleArn,
+        });
+    }
+}
+
+/**
+ * Wraps API call to get the data on the eks.Cluster. 
+ * @param clusterName 
+ * @param region 
+ * @returns 
+ */
+export async function getCluster(clusterName: string, region: string): Promise<sdk.Cluster> {
+    const client = new sdk.EKSClient({ region: process.env.CDK_DEFAULT_REGION });
+    const input: sdk.DescribeClusterRequest = {
+        name: clusterName
+    };
+
+    const command = new sdk.DescribeClusterCommand(input);
+    try {
+        const response = await client.send(command);
+        return response.cluster!;
+    }
+    catch (error) {
+        logger.error(error);
+        throw error;
     }
 }
--- lib/resource-providers/iam.ts
+++ lib/resource-providers/iam.ts
@@ -1,16 +1,18 @@
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { IManagedPolicy } from 'aws-cdk-lib/aws-iam';
 import * as spi from '../spi';
+import assert = require('assert');
 
 /**
  * Role provider that imports an existing role, performing its lookup by the provided name.
  */
 export class LookupRoleProvider implements spi.ResourceProvider<iam.IRole> {
 
-    constructor(private readonly roleName: string) { }
+    constructor(private readonly roleName: string, private readonly mutable?: boolean) { }
 
     provide(context: spi.ResourceContext): iam.IRole {
-        return iam.Role.fromRoleName(context.scope, `${this.roleName}-iam-provider`, this.roleName);
+        return iam.Role.fromRoleName(context.scope, `${this.roleName}-iam-provider`, this.roleName, 
+        { mutable: this.mutable });
     }
 
 }
@@ -33,5 +35,29 @@ export class CreateRoleProvider implements spi.ResourceProvider<iam.Role> {
             assumedBy: this.assumedBy,
             managedPolicies: this.policies
         });
+    }
+}
+
+const httpsPrefix = 'https://';
+
+/**
+ * OpenIdConnect provider can lookup an existing OpenIdConnectProvider based on the OIDC provider URL. 
+ */
+export class LookupOpenIdConnectProvider implements spi.ResourceProvider<iam.IOpenIdConnectProvider> {
+
+    protected readonly id: string;
+
+    constructor(readonly url: string, id?: string) {
+        const urlParts = url.split('/');
+        assert(url.startsWith(httpsPrefix) && urlParts[urlParts.length - 1], "Invalid OIDC provider URL format");
+        this.id = id ?? urlParts[urlParts.length - 1];
+    }
+
+    provide(context: spi.ResourceContext): iam.IOpenIdConnectProvider {
+        return iam.OpenIdConnectProvider.fromOpenIdConnectProviderArn(
+            context.scope,
+            this.id,
+            `arn:aws:iam::${context.scope.account}:oidc-provider/${this.url.substring(httpsPrefix.length)}`
+        )
     }
 }
