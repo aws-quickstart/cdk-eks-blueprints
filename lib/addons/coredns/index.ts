b94bd886f0f0ec852e16e12c567d0c441b094f1e
Howlla <bhavye.sharma1996@gmail.com>
2023-08-10 22:53:24 -0700

    coredns props extended to pass configurationValues

--- lib/addons/coredns/index.ts
+++ lib/addons/coredns/index.ts
@@ -1,15 +1,24 @@
-import { CoreAddOn } from "../core-addon";
+import { CoreAddOn, CoreAddOnProps } from "../core-addon";
+
+/**
+ * Configuration options for the coredns add-on.
+ */
+type corednsCollectorAddOnProps = CoreAddOnProps;
+
+
+const defaultProps = {
+    addOnName: 'coredns',
+    version: 'v1.9.3-eksbuild.5',
+    saName: 'coredns'
+}
 
 /**
  * Implementation of CoreDns EKS add-on.
  */
 export class CoreDnsAddOn extends CoreAddOn {
 
-    constructor(version?: string) {
-        super({
-            addOnName: "coredns",
-            version: version ?? "v1.9.3-eksbuild.5",
-            saName: "coredns"
-        });
+    constructor(props?: corednsCollectorAddOnProps) {
+        super({ ...defaultProps, ...props });
     }
+   
 }
