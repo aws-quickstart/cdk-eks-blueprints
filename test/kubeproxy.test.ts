75676e6597c11d69f3c417c9f55fb769de725fa3
Elamaran Shanmugam <elamaran.shan@gmail.com>
2024-01-10 21:05:24 -0500

    Upgrade Core Addons to support MAP of Versions for Kubernetes versions

--- test/kubeproxy.test.ts
+++ test/kubeproxy.test.ts
@@ -1,32 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
 import * as blueprints from '../lib';
 import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
 
-test("Kubeproxy Addon deploying correct default version of Addon for 1.28", () => {
+test("Kubeproxy Addon deploying correct default version of Addon for 1.28", async () => {
     const app = new cdk.App();
 
-    const stack = blueprints.EksBlueprint.builder()
+    await blueprints.EksBlueprint.builder()
         .account('123456789').region('us-west-2')
         .version(KubernetesVersion.V1_28)
         .addOns(new blueprints.KubeProxyAddOn("auto"))
         .build(app, "KubeProxy-stack-001");
 });
 
-test("Kubeproxy Addon deploying correct default version of Addon for 1.27", () => {
+test("Kubeproxy Addon deploying correct default version of Addon for 1.27", async () => {
     const app = new cdk.App();
 
-    const stack = blueprints.EksBlueprint.builder()
+    await blueprints.EksBlueprint.builder()
         .account('123456789').region('us-west-2')
         .version(KubernetesVersion.V1_27)
         .addOns(new blueprints.KubeProxyAddOn("auto"))
         .build(app, "KubeProxy-stack-002");
 });
 
-test("Kubeproxy Addon deploying correct default version of Addon for 1.26", () => {
+test("Kubeproxy Addon deploying correct default version of Addon for 1.26", async () => {
     const app = new cdk.App();
 
-    const stack = blueprints.EksBlueprint.builder()
+    await blueprints.EksBlueprint.builder()
         .account('123456789').region('us-west-2')
         .version(KubernetesVersion.V1_26)
         .addOns(new blueprints.KubeProxyAddOn("auto"))
