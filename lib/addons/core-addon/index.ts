968f55fce5ade7a0dceba13191394b0d99704fdc
Elamaran Shanmugam <elamaran.shan@gmail.com>
2024-01-17 11:27:56 -0500

    Upgrade Core Addons to support MAP of Versions for Kubernetes versions - Fixing Tests

--- lib/addons/core-addon/index.ts
+++ lib/addons/core-addon/index.ts
@@ -6,10 +6,7 @@ import { IManagedPolicy, ManagedPolicy, PolicyDocument } from "aws-cdk-lib/aws-i
 import { KubernetesVersion } from "aws-cdk-lib/aws-eks";
 import { createServiceAccountWithPolicy, deployBeforeCapacity, logger, userLog,  } from "../../utils";
 import * as sdk from "@aws-sdk/client-eks";
-<<<<<<< HEAD
 import { RemovalPolicy } from "aws-cdk-lib";
-=======
->>>>>>> 2db90598 (Upgrade Core Addons to support MAP of Versions for Kubernetes versions)
 
 export class CoreAddOnProps {
     /**
