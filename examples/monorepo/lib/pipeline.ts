d82818a447e252b76431a8dd408c42e98ebd0129
shapirov103 <shapirov@amazon.com>
2024-02-13 00:18:10 -0500

    monorepo test

---
+++ examples/monorepo/lib/pipeline.ts
@@ -0,0 +1,39 @@
+import { App } from 'aws-cdk-lib';
+import * as blueprints from '../../../lib';
+import { logger } from '../../../lib/utils';
+
+export function build(app: App) {
+    const account = process.env.CDK_DEFAULT_ACCOUNT!;
+    const region = 'us-west-2';
+    
+    logger.settings.minLevel = 3;
+    blueprints.HelmAddOn.validateHelmVersions = true;
+
+    const blueprint = blueprints.EksBlueprint.builder()
+        .account(account) // the supplied default will fail, but build and synth will pass
+        .region(region)
+        .version('auto')
+        .addOns(
+            new blueprints.AwsLoadBalancerControllerAddOn,
+            new blueprints.CertManagerAddOn,
+            new blueprints.AdotCollectorAddOn,
+            new blueprints.NginxAddOn,
+        );
+
+    blueprints.CodePipelineStack.builder()
+        .application("npx ts-node bin/pipeline.ts")
+        .name("blueprints-eks-pipeline")
+        .owner("aws-quickstart")
+        .codeBuildPolicies(blueprints.DEFAULT_BUILD_POLICIES)
+        .repository({
+            repoUrl: 'cdk-eks-blueprints',
+            credentialsSecretName: 'github-token',
+            targetRevision: 'bug/broken-monorepo-pipelines',
+            path: "examples/monorepo"
+        })
+        .stage({
+            id: 'us-west-2-sandbox',
+            stackBuilder: blueprint.clone('us-west-2')
+        })
+        .build(app, "pipeline", { env: { region, account }});
+}
