f4fa3d13b4d0692dbfa586f68d14052b4f02edea
youngjeong46 <youngjeong46@gmail.com>
2024-03-08 17:15:54 +0000

    fixing build and lint errors

--- lib/addons/karpenter/index.ts
+++ lib/addons/karpenter/index.ts
@@ -1,6 +1,6 @@
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from "constructs";
-import merge from 'ts-deepmerge';
+import { merge } from 'ts-deepmerge';
 import { ClusterInfo, NodePoolSpec, Ec2NodeClassSpec } from '../../spi';
 import * as utils from '../../utils';
 import { HelmAddOn, HelmAddOnProps, HelmAddOnUserProps } from '../helm-addon';
