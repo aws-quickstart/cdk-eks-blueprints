600c2853889b2cdcdfd12f8c52e1bee58e051ec7
Elamaran Shanmugam <elamaran.shan@gmail.com>
2024-04-22 16:33:46 -0400

    Adding Dev Containers

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# For Kubectl AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# For Kubectl ARM64
+[ $(uname -m) = aarch64 ] && curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# For Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+echo "Installing 'helm' utility ..."
+./get_helm.sh
+rm -rf get_helm.sh
+
+# setup autocomplete for kubectl and alias k
+mkdir $HOME/.kube
+echo "source <(kubectl completion bash)" >> $HOME/.bashrc
+echo "alias k=kubectl" >> $HOME/.bashrc
+echo "complete -F __start_kubectl k" >> $HOME/.bashrc
