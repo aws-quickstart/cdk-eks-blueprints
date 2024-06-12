39e3457213d9c9ecdc2fdfa3ceb550207d5b7cc9
Ahmed Ghanem <pro.ahmedghanem@gmail.com>
2024-06-12 18:03:49 +0100

    Remove: kubeshark from index.ts

--- lib/addons/index.ts
+++ lib/addons/index.ts
@@ -60,7 +60,6 @@ export * from './apache-airflow';
 export * from './neuron';
 export * from './eks-pod-identity-agent';
 export * from './neuron';
-export * from './kubeshark';
 
 export class Constants {
     public static readonly BLUEPRINTS_ADDON = "blueprints-addon";
