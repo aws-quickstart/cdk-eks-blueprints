cfbe78cec8ab3ebe5b9f510ac5ae56335b7aa1d3
bugathagit <ramesh.bugatha78@gmail.com>
2024-06-28 11:40:47 -0700

    IPV6 EKS cluster code.

--- bin/main.ts
+++ bin/main.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import BlueprintConstruct from '../examples/blueprint-construct';
+import BlueprintIPV6Construct from '../examples/blueprint-ipv6-construct';
 
 const app = new cdk.App();
 
@@ -9,3 +10,6 @@ const region = process.env.CDK_DEFAULT_REGION;
 const props = { env: { account, region } };
 
 new BlueprintConstruct(app, props);
+
+// Create ipv6 cluster
+new BlueprintIPV6Construct(app, props);
