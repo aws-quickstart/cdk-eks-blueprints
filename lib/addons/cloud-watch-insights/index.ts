57e77286c54da3e0ffc6f7b7f271d1332ce760ec
shapirov <shapirov@amazon.com>
2024-10-09 10:37:20 -0400

    Fixed tests, build issues, upgraded CDK and libs

--- lib/addons/cloud-watch-insights/index.ts
+++ lib/addons/cloud-watch-insights/index.ts
@@ -10,11 +10,10 @@ import {KubernetesVersion} from "aws-cdk-lib/aws-eks";
 // aws eks describe-addon-versions --kubernetes-version <kubernetes-version> --addon-name amazon-cloudwatch-observability \
 //     --query 'addons[].addonVersions[].{Version: addonVersion, Defaultversion: compatibilities[0].defaultVersion}' --output table
 const versionMap: Map<KubernetesVersion, string> = new Map([
-  [KubernetesVersion.V1_30, "v1.10.0-eksbuild.2"],
-  [KubernetesVersion.V1_29, "v1.10.0-eksbuild.2"],
-  [KubernetesVersion.V1_28, "v1.10.0-eksbuild.2"],
-  [KubernetesVersion.V1_27, "v1.10.0-eksbuild.2"],
-  [KubernetesVersion.V1_26, "v1.10.0-eksbuild.2"],
+  [KubernetesVersion.V1_30, "v2.1.2-eksbuild.1"],
+  [KubernetesVersion.V1_29, "v2.1.2-eksbuild.1"],
+  [KubernetesVersion.V1_28, "v2.1.2-eksbuild.1"],
+  [KubernetesVersion.V1_27, "v2.1.2-eksbuild.1"]
 ]);
 
 
