022aa97dd39c0a0340c38b83bf7512ccb99bd525
Jesper Almstrom <jesper.almstrom@gmail.com>
2024-10-24 00:00:18 +0200

    Add S3 CSI driver IAM policy as addon
    This commit adds a new file `iam-policy.ts` that contains the IAM policy statements for the S3 CSI driver. It also adds a new file `index.ts` that implements the S3 CSI driver addon. The addon creates a service account, attaches the IAM policy to the service account's role, and deploys the addon to the cluster. The commit also includes some helper functions and default options for the addon.

---
+++ lib/addons/s3-csi-driver/iam-policy.ts
@@ -0,0 +1,24 @@
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export function getS3DriverPolicyStatements(s3BucketArn: string): iam.PolicyStatement[] {
+    // new IAM policy to grand access to S3 bucket
+    // https://github.com/awslabs/mountpoint-s3/blob/main/doc/CONFIGURATION.md#iam-permissions
+    return [
+        new iam.PolicyStatement({
+            sid: 'S3MountpointFullBucketAccess',
+            actions: [
+                "s3:ListBucket"
+            ],
+            resources: [s3BucketArn]
+        }),
+        new iam.PolicyStatement({
+            sid: 'S3MountpointFullObjectAccess',
+            actions: [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:AbortMultipartUpload",
+                "s3:DeleteObject"
+            ],
+            resources: [`${s3BucketArn}/*`]
+        })];
+}
