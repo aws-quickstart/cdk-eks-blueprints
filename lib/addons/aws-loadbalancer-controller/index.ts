1d6f32bde77d16b1d744e3a4ea02ae50ff0d6ebc
shapirov103 <shapirov@amazon.com>
2024-12-20 15:48:52 -0500

    Fixing S3 CSI driver and minor improvements in tests

--- lib/addons/aws-loadbalancer-controller/index.ts
+++ lib/addons/aws-loadbalancer-controller/index.ts
@@ -57,7 +57,7 @@ const defaultProps: AwsLoadBalancerControllerProps = {
     chart: AWS_LOAD_BALANCER_CONTROLLER,
     repository: 'https://aws.github.io/eks-charts',
     release: AWS_LOAD_BALANCER_CONTROLLER,
-    version: '1.9.2',
+    version: '1.11.0',
     enableShield: false,
     enableWaf: false,
     enableWafv2: false,
--- lib/addons/efs-csi-driver/index.ts
+++ lib/addons/efs-csi-driver/index.ts
@@ -42,7 +42,7 @@ export interface EfsCsiDriverProps extends HelmAddOnUserProps {
  * Defaults options for the add-on
  */
 const defaultProps: EfsCsiDriverProps = {
-    version: '3.0.8',
+    version: '3.1.3',
     namespace: "kube-system",
     repository: "https://kubernetes-sigs.github.io/aws-efs-csi-driver/",
     name: EFS_CSI_DRIVER,
