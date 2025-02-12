6dcd638f479acc35c46baa3ca6937b7449c045ec
Usman <usman@lomax.io>
2025-02-12 10:58:45 +0000

    fix: one more rename

--- lib/addons/s3-csi-driver/iam-policy.ts
+++ lib/addons/s3-csi-driver/iam-policy.ts
@@ -3,14 +3,14 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 export function getS3DriverPolicyStatements(bucketNames: string[]): iam.PolicyStatement[] {
     // new IAM policy to grand access to S3 bucket
     // https://github.com/awslabs/mountpoint-s3/blob/main/doc/CONFIGURATION.md#iam-permissions
-    const s3BucketArns = bucketNames.map((name) => `arn:aws:s3:::${name}`);
+    const arns = bucketNames.map((name) => `arn:aws:s3:::${name}`);
     return [
         new iam.PolicyStatement({
             sid: 'S3MountpointFullBucketAccess',
             actions: [
                 "s3:ListBucket"
             ],
-            resources: s3BucketArns
+            resources: arns
         }),
         new iam.PolicyStatement({
             sid: 'S3MountpointFullObjectAccess',
@@ -20,6 +20,6 @@ export function getS3DriverPolicyStatements(bucketNames: string[]): iam.PolicySt
                 "s3:AbortMultipartUpload",
                 "s3:DeleteObject"
             ],
-            resources: s3BucketArns.map((arn) => `${arn}/*`)
+            resources: arns.map((arn) => `${arn}/*`)
         })];
 }
