225cd6c410dfba94746a789fbffcec572e92497b
Zachary Jacobson <zjacobso@amazon.com>
2026-07-15 16:04:15 -0400

    add kubectl 1.36 lambda layer

--- lib/cluster-providers/generic-cluster-provider.ts
+++ lib/cluster-providers/generic-cluster-provider.ts
@@ -5,6 +5,7 @@ import { KubectlV32Layer } from "@aws-cdk/lambda-layer-kubectl-v32";
 import { KubectlV33Layer } from "@aws-cdk/lambda-layer-kubectl-v33";
 import { KubectlV34Layer } from "@aws-cdk/lambda-layer-kubectl-v34";
 import { KubectlV35Layer } from "@aws-cdk/lambda-layer-kubectl-v35";
+import { KubectlV36Layer } from "@aws-cdk/lambda-layer-kubectl-v36";
 
 import { Tags } from "aws-cdk-lib";
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
@@ -47,13 +48,15 @@ export function selectKubectlLayer(scope: Construct, version: eks.KubernetesVers
             return new KubectlV34Layer(scope, "kubectllayer34");
         case "1.35":
             return new KubectlV35Layer(scope, "kubectllayer35");
+        case "1.36":
+            return new KubectlV36Layer(scope, "kubectllayer36")
 
     }
 
     const minor = version.version.split('.')[1];
 
-    if(minor && parseInt(minor, 10) > 35) {
-        return new KubectlV35Layer(scope, "kubectllayer35"); // for all versions above 1.35 use 1.35 kubectl (unless explicitly supported in CDK)
+    if(minor && parseInt(minor, 10) > 36) {
+        return new KubectlV36Layer(scope, "kubectllayer36"); // for all versions above 1.35 use 1.35 kubectl (unless explicitly supported in CDK)
     }
     return undefined;
 }
